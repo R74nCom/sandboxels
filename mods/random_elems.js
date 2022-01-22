d9d1f8979c7524f554ccb5c20d83a5ad6e3fa85b
Lily-129 <68935009+Lily-129@users.noreply.github.com>
2022-01-22 18:36:30 -0500

    fairy restrictions
    no more fairy floods

--- mods/random_elems.js
+++ mods/random_elems.js
@@ -221,6 +221,19 @@ elements.QodfMdFo = {
     density: 11140.091847406393,
 }
 
+elements.rWWkMgJN = {
+    color: "#2eeaa0",
+    behavior: [
+        "CR:ant|CH:salt%7|DB",
+        "DL:freezer|FX|XX",
+        "CC:#c82b89|BO|HT:5",
+    ],
+    category: "random",
+    conduct: 0.698741020714595,
+    tempHigh: 2686,
+    density: 17409.619803480866,
+}
+
 runAfterLoad(function() {
 	if(enabledMods.includes("mods/chalcopyrite.js") && enabledMods.includes("mods/extra_element_info.js") && enabledMods.includes("mods/fey_and_more.js") && enabledMods.includes("mods/ketchup_mod.js") && enabledMods.includes("mods/test.js") && enabledMods.includes("mods/randomness.js") && enabledMods.includes("mods/Neutronium Mod.js") && enabledMods.includes("mods/test2.js") && enabledMods.includes("mods/fairy_chain.js") && enabledMods.includes("mods/cum.js") && enabledMods.includes("mods/random_elems.js") && enabledMods.includes("mods/sus.js")) {
 		elements.yckRMplh = {
@@ -231,19 +244,18 @@ runAfterLoad(function() {
 				"BO|SH|M2",
 			],
 			reactions: {
-				"fairy98": { "elem1":"molten_uranium", "elem2":"tritium", "chance":0.4602898017566186 },
+				"fairy_fairy_fairy_fairy": { "elem1":"molten_uranium", "elem2":"tritium", "chance":0.4602898017566186 },
 				"fluxed_matte_copper": { "elem1":"pure_water", "elem2":"enchanted_ketchup", "chance":0.8272186749812657 },
 				"molten_neutronium": { "elem1":"amogus9", "elem2":"fallout", "chance":0.9302153671106638 },
 				"bone_beast": { "elem1":"celie_leaves", "elem2":"dirt", "chance":0.3741482072147079 },
-				"fairy27": { "elem1":"led_g", "elem2":"smoke", "chance":0.29352648263376613 },
-				"fairy92": { "elem1":"old_celie_leaves", "elem2":"green_dye", "chance":0.5993662901794029 },
+				"fairy_fairy": { "elem1":"led_g", "elem2":"smoke", "chance":0.29352648263376613 },
+				"fairy_fairy_fairy": { "elem1":"old_celie_leaves", "elem2":"green_dye", "chance":0.5993662901794029 },
 				"flamer": { "elem1":"amogus1", "elem2":"brass", "chance":0.5319076167877976 },
 			},
 			category: "random",
 			tempHigh: 537,
 			density: 9215.024069840034,
 		}
-
 		elements.urhhhqJS = {
 			color: "#31a6bf",
 			behavior: [
@@ -254,7 +266,6 @@ runAfterLoad(function() {
 			category: "random",
 			density: 373.9243313453792,
 		}
-
 		elements.BUDGKClj = {
 			color: "#ba657f",
 			behavior: [
@@ -296,13 +307,157 @@ runAfterLoad(function() {
 			color: "#1bb24d",
 			behavior: [
 				"SW:fly|SW|CR:burning_unnamed_powder",
-				"CO:9|CC:#71a6b1|CH:fairy100>amogus3",
+				"CO:9|CC:#71a6b1|CH:fairy_fairy_fairy_fairy>amogus3",
 				"BO|XX|CH:bone%6",
 			],
 			category: "random",
 			conduct: 0.18249512087981343,
 			tempHigh: 2926,
 			density: 5349.2882949054265,
+		}
+		elements.ACsjdBSP = {
+			color: "#c25375",
+			behavior: [
+				"CH:antigas>fairy_fairy_fairy_fairy|BO|XX",
+				"DL:chocolate|CH:fairy_fairy_fairy%10|BO",
+				"SW:tree_branch|XX|SW:tree_branch",
+			],
+			category: "random",
+			density: 19429.0198815719,
+		}
+		elements.BXXBBUgd = {
+			color: "#63fb15",
+			behavior: [
+				"DB|SH|DB",
+				"XX|XX|SW:aluminum",
+				"SW|SW|CH:a4left%7",
+			],
+			category: "random",
+			density: 11933.76730202344,
+		}
+		elements.KkHXGSiW = {
+			color: "#30ee04",
+			behavior: [
+				"SW:molten_copper|CH:fairy_fairy_fairy%8|SW:molten_copper",
+				"XX|DL%5|ST",
+				"CH:dead_cum>molten_tungstensteel|DB|DL:fairy",
+			],
+			reactions: {
+				"led_r": { "elem1":"ketchup_metal", "elem2":"mystic_fire", "chance":0.032394734075084766 },
+				"plague": { "elem1":"flameshockwave3", "elem2":"stardust", "chance":0.13908761952404325 },
+				"blood": { "elem1":"rocket", "elem2":"fairy_fairy_fairy", "chance":0.16814629599115316 },
+				"yeast": { "elem1":"oil", "elem2":"juice", "chance":0.9787877797027756 },
+				"stardust": { "elem1":"molten_AyvnmLRQ", "elem2":"fairy_fairy", "chance":0.18375417180105702 },
+				"cooler": { "elem1":"celie_leaves", "elem2":"corn_seed", "chance":0.15359338811668444 },
+			},
+			category: "random",
+			burn: 60,
+			burnTime: 491,
+			density: 6333.626319914362,
+		}
+		elements.GQYHBMuQ = {
+			color: "#3e3969",
+			behavior: [
+				"CR:flea|CH:led_b%5|BO",
+				"SW|CC:#aaf599|SW:molten_dirt",
+				"SW|ST|SW",
+			],
+			category: "random",
+			tempHigh: 2972,
+			density: 3881.977856937143,
+		}
+		elements.OOKIoObo = {
+			color: "#f072da",
+			behavior: [
+				"SW|SW|DL:fairy_fairy_fairy_fairy",
+				"RT|XX|SW:copper",
+				"BO|XX|XX",
+			],
+			category: "random",
+			density: 34.228020163470646,
+		}
+		elements.qQNuEjNT = {
+			color: "#0a4f5d",
+			behavior: [
+				"BO|CH:tin>udder|ST",
+				"CH:molten_amogus7%1|DL%9|SW:magma",
+				"M1|XX|XX",
+			],
+			category: "random",
+			burn: 47,
+			burnTime: 183,
+			tempHigh: 2096,
+			density: 9679.340844240462,
+		}
+		elements.HgSHruqE = {
+			color: "#d59786",
+			behavior: [
+				"HT:3|M1|RT",
+				"XX|FY|RT",
+				"M2|CC:#885997|M1",
+			],
+			category: "random",
+			burn: 85,
+			burnTime: 90,
+			tempHigh: 346,
+			density: 11697.491509311361,
+		}
+		elements.hyVrNXQQ = {
+			color: "#a214b9",
+			behavior: [
+				"SW|XX|CH:dead_cum_water%2",
+				"CO:4|LB:molten_amogus4|CH:fairy_fairy>molten_potassium_salt",
+				"CH:fairy_fairy_fairy_fairy>molten_potassium_salt|CR:rainbow|CR:rainbow",
+			],
+			reactions: {
+				"fairy_fairy_fairy": { "elem1":"concoction", "elem2":"straw", "chance":0.17509068546150386 },
+			},
+			category: "random",
+			burn: 69,
+			burnTime: 76,
+			tempHigh: 1122,
+			density: 7158.798415310327,
+		}
+		elements.xxYsTXnM = {
+			color: "#763817",
+			behavior: [
+				"XX|SW|CC:#210b1d",
+				"SW:blue_dye|CO:6|SW:blue_dye",
+				"SW:blue_dye|HT:9|XX",
+			],
+			category: "random",
+			tempHigh: 2187,
+			density: 15160.604263202209,
+		}
+		elements.SowljGtd = {
+			color: "#c554eb",
+			behavior: [
+				"BO|XX|DL:t2",
+				"XX|FY|XX",
+				"XX|SW:ionized_hydrogen|HT:8",
+			],
+			reactions: {
+				"petal": { "elem1":"fairy_fairy_fairy", "elem2":"molten_sterling", "chance":0.13858328633550154 },
+				"fairy_fairy_fairy_fairy": { "elem1":"glass_shard", "elem2":"oxidized_copper", "chance":0.8228073223526745 },
+				"molten_corrupt_land": { "elem1":"everfire_dust", "elem2":"matte_copper", "chance":0.6538956182983582 },
+				"tralphium": { "elem1":"ice_fairy", "elem2":"mycelium", "chance":0.015319150698328909 },
+				"tritium": { "elem1":"molten_zinc", "elem2":"fairy_fairy", "chance":0.13333787328877345 },
+			},
+			category: "random",
+			burn: 82,
+			conduct: 0.5267298783471723,
+			burnTime: 313,
+			density: 10531.689830022671,
+		}
+		elements.giVRBDjd = {
+			color: "#c08adf",
+			behavior: [
+				"SH|DB|M1",
+				"BO|CH:slug%9|XX",
+				"M1|RT|SP",
+			],
+			category: "random",
+			density: 8965.264917972576,
 		}
 	};
 });
