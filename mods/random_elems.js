18d18780d0315dce4d78219609c4488942481eb9
Lily-129 <68935009+Lily-129@users.noreply.github.com>
2022-01-22 01:00:58 -0500

    randomly generated elements
    why

---
+++ mods/random_elems.js
@@ -0,0 +1,210 @@
+elements.ogqWWRKo = {
+    color: "#b5f134",
+    behavior: [
+        "DB|XX|XX",
+        "CH:molten_silver>antifire|XX|BO",
+        "SA|SA|XX",
+    ],
+    category: "random",
+    burn: 57,
+    burnTime: 451,
+    tempHigh: 1055,
+    density: 6632.184014557264,
+}
+
+elements.KIBiFwpD = {
+    color: "#b8a3a5",
+    behavior: [
+        "XX|RT|CH:light%32",
+        "SW|DL%25|SH",
+        "DB|SH|SW:firefly",
+    ],
+    category: "random",
+    density: 1382.2019914825285,
+}
+
+elements.SEzhorqQ = {
+    color: "#f8f19c",
+    behavior: [
+        "DB|XX|M1",
+        "SA|CH:wheat%23|ST",
+        "M1|CH:molten_bronze>charcoal|XX",
+    ],
+    category: "random",
+    burn: 26,
+    burnTime: 228,
+    density: 11054.763724436774,
+}
+
+elements.bGceLthV = {
+    color: "#2a8885",
+    behavior: [
+        "CC:#ec80f8|XX|ST",
+        "SW:salt|HT:10|RT",
+        "CH:glitter%4|SH|SW:salt",
+    ],
+    reactions: {
+        "meat": { "elem1":"mushroom_stalk", "elem2":"bee", "chance":0.07255393753591068 },
+        "ball": { "elem1":"frog", "elem2":"acid_gas", "chance":0.8120527233695284 },
+        "antimatter": { "elem1":"molten_salt", "elem2":"dirty_water", "chance":0.18656242174277482 },
+        "burner": { "elem1":"electric", "elem2":"charcoal", "chance":0.9864527094278326 },
+        "oil": { "elem1":"epsom_salt", "elem2":"bamboo_plant", "chance":0.7298237285611298 },
+    },
+    category: "random",
+    density: 1446.9471325970142,
+}
+
+elements.hpWfukAK = {
+    color: "#f22c87",
+    behavior: [
+        "XX|SW:bronze|SW",
+        "CR:cancer|LB:slug|CO:1",
+        "BO|SW:bronze|BO",
+    ],
+    category: "random",
+    burn: 71,
+    burnTime: 449,
+    density: 10877.887548154851,
+}
+
+elements.GgYSMbID = {
+    color: "#81ff73",
+    behavior: [
+        "SW|CR:molten_sterling|BO",
+        "DL:magma|C2:wood|XX",
+        "CC:#de4dbe|XX|DL:magma",
+    ],
+    reactions: {
+        "slug": { "elem1":"mushroom_cap", "elem2":"plastic", "chance":0.7318844051213007 },
+        "wheat": { "elem1":"frozen_meat", "elem2":"mushroom_spore", "chance":0.7895678110819928 },
+        "sulfur": { "elem1":"caramel", "elem2":"liquid_hydrogen", "chance":0.8659314154325147 },
+        "bamboo_plant": { "elem1":"magma", "elem2":"glitter", "chance":0.5424222221105207 },
+    },
+    category: "random",
+    tempHigh: 2956,
+    density: 6284.617729241624,
+}
+
+elements.FpNkluKG = {
+    color: "#a68793",
+    behavior: [
+        "DB|DL:calcium|DB",
+        "SW|LB:algae|ST",
+        "DL:calcium|HT:4|CR:copper",
+    ],
+    reactions: {
+        "dirt": { "elem1":"sand", "elem2":"water", "chance":0.9366010380552752 },
+    },
+    category: "random",
+    tempHigh: 1300,
+    density: 3828.9615486132043,
+}
+
+elements.EfgTDvzx = {
+    color: "#96ca55",
+    behavior: [
+        "SA|CO:7|SH",
+        "SW:potato_seed|L2:slaked_lime|CH:sugar_water>soap",
+        "BO|CC:#0bd2dd|CO:7",
+    ],
+    category: "random",
+    density: 18274.111700368052,
+}
+
+elements.QACWIVPp = {
+    color: "#8f57c0",
+    behavior: [
+        "HT:8|CH:quicklime>copper|CR:dioxin",
+        "CC:#f64cc7|L1:mudstone|M2",
+        "CH:quicklime>copper|RT|CH:firefly%3",
+    ],
+    category: "random",
+    density: 12926.310301027497,
+}
+
+elements.qFgDRSAY = {
+    color: "#c06b4e",
+    behavior: [
+        "CC:#bb68ee|DB|DL:molten_brick",
+        "CR:iron|CO:1|M1",
+        "CO:1|M2|SP",
+    ],
+    category: "random",
+    burn: 50,
+    burnTime: 376,
+    density: 17821.827734771083,
+}
+
+elements.QbeSGuOy = {
+    color: "#4b9513",
+    behavior: [
+        "CC:#39d412|CO:10|CO:10",
+        "HT:5|FY|SW:snow_cloud",
+        "M1|SH|CO:10",
+    ],
+    reactions: {
+        "ant": { "elem1":"tin", "elem2":"acid", "chance":0.015683479804684108 },
+        "liquid_hydrogen": { "elem1":"molten_calcium", "elem2":"tinder", "chance":0.5541216729460897 },
+        "molten_borax": { "elem1":"wall", "elem2":"bee", "chance":0.037733528661837115 },
+        "rock": { "elem1":"wire", "elem2":"wall", "chance":0.5515341715699367 },
+        "chocolate": { "elem1":"wet_sand", "elem2":"bamboo_plant", "chance":0.34082969121459494 },
+        "vinegar": { "elem1":"rock", "elem2":"flour", "chance":0.3551432786489651 },
+        "molten_iron": { "elem1":"led_r", "elem2":"udder", "chance":0.2856279501232143 },
+        "floating_cloner": { "elem1":"udder", "elem2":"copper", "chance":0.3669477762812875 },
+        "cancer": { "elem1":"yeast", "elem2":"bone_marrow", "chance":0.7241288621811043 },
+    },
+    category: "random",
+    burn: 34,
+    burnTime: 408,
+    density: 7097.343018249547,
+}
+
+elements.oMsDvDdH = {
+    color: "#e2d948",
+    behavior: [
+        "HT:8|RT|M1",
+        "ST|L1:chocolate_syrup|M2",
+        "SA|CR:flea|SH",
+    ],
+    category: "random",
+    burn: 98,
+    conduct: 0.6023663023285293,
+    burnTime: 199,
+    tempHigh: 2767,
+    density: 10910.501840659097,
+}
+
+elements.IzhSEMYl = {
+    color: "#7fafff",
+    behavior: [
+        "ST|SW:clay_soil|CO:11",
+        "SA|LB:infection|BO",
+        "SW:clay_soil|SW:clay_soil|CR:dough",
+    ],
+    category: "random",
+    density: 2810.029487411252,
+}
+
+elements.AyvnmLRQ = {
+    color: "#668a95",
+    behavior: [
+        "HT:1|M2|SA",
+        "CR:molten_nickel|CH:light%6|CO:4",
+        "SP|HT:1|SW:liquid_oxygen",
+    ],
+    category: "random",
+    tempHigh: 893,
+    density: 11509.655636160245,
+}
+
+elements.pJRDtuTA = {
+    color: "#b12040",
+    behavior: [
+        "BO|SW:sodium_acetate|CO:1",
+        "BO|L1:laser|CC:#e40c6e",
+        "SW:sodium_acetate|SW|BO",
+    ],
+    category: "random",
+    conduct: 0.33534106397322677,
+    density: 18826.565148369646,
+}
