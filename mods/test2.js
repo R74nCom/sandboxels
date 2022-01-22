0eb555d273c40c68df43a0c67cb4f47f63c8a5ac
Lily-129 <68935009+Lily-129@users.noreply.github.com>
2022-01-21 22:15:13 -0500

    made amogus3 green

--- mods/test2.js
+++ mods/test2.js
@@ -1,335 +1,335 @@
 //right col
 elements.molten_amogus1 = {
     behavior: [
-		"CH:amogus1>null|CR:amogus2 AND CH:amogus2 AND CH:amogus1>null|CH:amogus1>null",
-		"M2 AND CH:amogus1>null|XX|M2",
-		"M1 AND CH:amogus1>null|M1|M1",
-	],
+        "CH:amogus1>null|CR:amogus2 AND CH:amogus2 AND CH:amogus1>null|CH:amogus1>null",
+        "M2 AND CH:amogus1>null|XX|M2",
+        "M1 AND CH:amogus1>null|M1|M1",
+    ],
     category: "special",
     state: "liquid",
     density: (6942.0*0.9),
-	hidden: true,
+    hidden: true,
 };
 elements.molten_amogus2 = {
     behavior: [
-		"XX|CR:amogus3 AND CH:amogus3|M2",
-		"M2 AND CR:amogus5 AND CH:amogus5|DL%25|M2",
-		"XX|XX|XX",
-	],
+        "XX|CR:amogus3 AND CH:amogus3|M2",
+        "M2 AND CR:amogus5 AND CH:amogus5|DL%25|M2",
+        "XX|XX|XX",
+    ],
     category: "special",
     state: "liquid",
     density: (6942.0*0.9),
-	hidden: true,
+    hidden: true,
 };
 elements.molten_amogus3 = {
     behavior: [
-		"XX|CR:amogus4 AND CH:amogus4|XX",
-		"M2 AND CR:amogus6 AND CH:amogus6|DL%25|M2",
-		"XX|XX|XX",
-	],
+        "XX|CR:amogus4 AND CH:amogus4|XX",
+        "M2 AND CR:amogus6 AND CH:amogus6|DL%25|M2",
+        "XX|XX|XX",
+    ],
     category: "special",
     state: "liquid",
     density: (6942.0*0.9),
-	hidden: true,
+    hidden: true,
 };
 elements.molten_amogus4 = {
     behavior: [
-		"XX|XX|XX",
-		"M2 AND CR:amogus7 AND CH:amogus7|DL%25|M2",
-		"XX|XX|XX",
-	],
+        "XX|XX|XX",
+        "M2 AND CR:amogus7 AND CH:amogus7|DL%25|M2",
+        "XX|XX|XX",
+    ],
     category: "special",
     state: "liquid",
     density: (6942.0*0.9),
-	hidden: true,
+    hidden: true,
 };
 //middle col
 elements.molten_amogus5 = {
     behavior: [
-		"XX|XX|XX",
-		"M2 AND CR:amogus9 AND CH:amogus9|DL%25|M2",
-		"XX|XX|XX",
-	],
+        "XX|XX|XX",
+        "M2 AND CR:amogus9 AND CH:amogus9|DL%25|M2",
+        "XX|XX|XX",
+    ],
     category: "specials",
     state: "liquid",
     density: (6942.0*0.9),
-	hidden: true,
+    hidden: true,
 };
 elements.molten_amogus6 = {
     behavior: [
-		"XX|XX|XX",
-		"M2 AND CR:amogus10 AND CH:amogus10|DL%25|M2",
-		"XX|XX|XX",
-	],
+        "XX|XX|XX",
+        "M2 AND CR:amogus10 AND CH:amogus10|DL%25|M2",
+        "XX|XX|XX",
+    ],
     category: "specials",
     state: "liquid",
     density: (6942.0*0.9),
-	hidden: true,
+    hidden: true,
 };
 elements.molten_amogus7 = {
     behavior: [
-		"XX|XX|XX",
-		"M2 AND CR:amogus11 AND CH:amogus11|DL%25|M2",
-		"XX|XX|XX",
-	],
+        "XX|XX|XX",
+        "M2 AND CR:amogus11 AND CH:amogus11|DL%25|M2",
+        "XX|XX|XX",
+    ],
     category: "specials",
     state: "liquid",
     density: (6942.0*0.9),
-	hidden: true,
+    hidden: true,
 };
 //left col
 elements.molten_amogus9 = {
     behavior: [
-		"XX|CR:amogus10 AND CH:amogus10|XX",
-		"M2|DL%25|M2",
-		"XX|CR:amogus8 AND CH:amogus8|XX",
-	],
+        "XX|CR:amogus10 AND CH:amogus10|XX",
+        "M2|DL%25|M2",
+        "XX|CR:amogus8 AND CH:amogus8|XX",
+    ],
     category: "specials",
     state: "liquid",
     density: (6942.0*0.9),
-	hidden: true,
+    hidden: true,
 };
 elements.molten_amogus8 = {
     behavior: [
-		"XX|XX|XX",
-		"M2|DL%25|M2",
-		"XX|XX|XX",
-	],
+        "XX|XX|XX",
+        "M2|DL%25|M2",
+        "XX|XX|XX",
+    ],
     category: "specials",
     state: "liquid",
     density: (6942.0*0.9),
-	hidden: true,
+    hidden: true,
 };
 elements.molten_amogus10 = {
     behavior: [
-		"XX|CR:amogus11 AND CH:amogus11|XX",
-		"M2|DL%25|M2",
-		"XX|XX|XX",
-	],
+        "XX|CR:amogus11 AND CH:amogus11|XX",
+        "M2|DL%25|M2",
+        "XX|XX|XX",
+    ],
     category: "specials",
     state: "liquid",
     density: (6942.0*0.9),
-	hidden: true,
+    hidden: true,
 };
 elements.molten_amogus11 = {
     behavior: [
-		"XX|XX|XX",
-		"M2|DL%25|M2",
-		"XX|XX|XX",
-	],
+        "XX|XX|XX",
+        "M2|DL%25|M2",
+        "XX|XX|XX",
+    ],
     category: "specials",
     state: "liquid",
     density: (6942.0*0.9),
-	hidden: true,
+    hidden: true,
 };
 //right col
 elements.amogus1 = {
     color: "#ff0000",
     behavior: [
-		"CH:amogus1>null|CR:amogus2 AND CH:amogus2 AND CH:amogus1>null|CH:amogus1>null",
-		"CH:amogus1>null|XX|XX",
-		"M2 AND CH:amogus1>null|M1|M2",
-	],
+        "CH:amogus1>null|CR:amogus2 AND CH:amogus2 AND CH:amogus1>null|CH:amogus1>null",
+        "CH:amogus1>null|XX|XX",
+        "M2 AND CH:amogus1>null|M1|M2",
+    ],
     category: "special",
     state: "solid",
     density: 6942.0,
-	tempHigh: 420,
+    tempHigh: 420,
 };
 elements.amogus2 = {
     color: "#ff0000",
     behavior: [
-		"XX|CR:amogus3 AND CH:amogus3|XX",
-		"CR:amogus5 AND CH:amogus5|DL%25|XX",
-		"XX|XX|XX",
-	],
+        "XX|CR:amogus3 AND CH:amogus3|XX",
+        "CR:amogus5 AND CH:amogus5|DL%25|XX",
+        "XX|XX|XX",
+    ],
     category: "special",
     state: "solid",
     density: 6942.0,
-	hidden: true,
-	tempHigh: 420,
+    hidden: true,
+    tempHigh: 420,
 };
 elements.amogus3 = {
-    color: "#ff0000",
+    color: "#00ffff",
     behavior: [
-		"XX|CR:amogus4 AND CH:amogus4|XX",
-		"CR:amogus6 AND CH:amogus6|DL%25|XX",
-		"XX|XX|XX",
-	],
+        "XX|CR:amogus4 AND CH:amogus4|XX",
+        "CR:amogus6 AND CH:amogus6|DL%25|XX",
+        "XX|XX|XX",
+    ],
     category: "special",
     state: "solid",
     density: 6942.0,
-	hidden: true,
-	tempHigh: 420,
+    hidden: true,
+    tempHigh: 420,
 };
 elements.amogus4 = {
     color: "#ff0000",
     behavior: [
-		"XX|XX|XX",
-		"CR:amogus7 AND CH:amogus7|DL%25|XX",
-		"XX|XX|XX",
-	],
+        "XX|XX|XX",
+        "CR:amogus7 AND CH:amogus7|DL%25|XX",
+        "XX|XX|XX",
+    ],
     category: "special",
     state: "solid",
     density: 6942.0,
-	hidden: true,
-	tempHigh: 420,
+    hidden: true,
+    tempHigh: 420,
 };
 //middle col
 elements.amogus5 = {
     color: "#ff0000",
     behavior: [
-		"XX|XX|XX",
-		"CR:amogus9 AND CH:amogus9|DL%25|XX",
-		"XX|XX|XX",
-	],
+        "XX|XX|XX",
+        "CR:amogus9 AND CH:amogus9|DL%25|XX",
+        "XX|XX|XX",
+    ],
     category: "specials",
     state: "solid",
     density: 6942.0,
-	hidden: true,
-	tempHigh: 420,
+    hidden: true,
+    tempHigh: 420,
 };
 elements.amogus6 = {
     color: "#00ffff",
     behavior: [
-		"XX|XX|XX",
-		"CR:amogus10 AND CH:amogus10|DL%25|XX",
-		"XX|XX|XX",
-	],
+        "XX|XX|XX",
+        "CR:amogus10 AND CH:amogus10|DL%25|XX",
+        "XX|XX|XX",
+    ],
     category: "specials",
     state: "solid",
     density: 6942.0,
-	hidden: true,
-	tempHigh: 420,
+    hidden: true,
+    tempHigh: 420,
 };
 elements.amogus7 = {
     color: "#ff0000",
     behavior: [
-		"XX|XX|XX",
-		"CR:amogus11 AND CH:amogus11|DL%25|XX",
-		"XX|XX|XX",
-	],
+        "XX|XX|XX",
+        "CR:amogus11 AND CH:amogus11|DL%25|XX",
+        "XX|XX|XX",
+    ],
     category: "specials",
     state: "solid",
     density: 6942.0,
-	hidden: true,
-	tempHigh: 420,
+    hidden: true,
+    tempHigh: 420,
 };
 //left col
 elements.amogus9 = {
     color: "#ff0000",
     behavior: [
-		"XX|CR:amogus10 AND CH:amogus10|XX",
-		"XX|DL%25|XX",
-		"XX|CR:amogus8 AND CH:amogus8|XX",
-	],
+        "XX|CR:amogus10 AND CH:amogus10|XX",
+        "XX|DL%25|XX",
+        "XX|CR:amogus8 AND CH:amogus8|XX",
+    ],
     category: "specials",
     state: "solid",
     density: 6942.0,
-	hidden: true,
-	tempHigh: 420,
+    hidden: true,
+    tempHigh: 420,
 };
 elements.amogus8 = {
     color: "#ff0000",
     behavior: [
-		"XX|XX|XX",
-		"XX|DL%25|XX",
-		"XX|XX|XX",
-	],
+        "XX|XX|XX",
+        "XX|DL%25|XX",
+        "XX|XX|XX",
+    ],
     category: "specials",
     state: "solid",
     density: 6942.0,
-	hidden: true,
-	tempHigh: 420,
+    hidden: true,
+    tempHigh: 420,
 };
 elements.amogus10 = {
     color: "#ff0000",
     behavior: [
-		"XX|CR:amogus11 AND CH:amogus11|XX",
-		"XX|DL%25|XX",
-		"XX|XX|XX",
-	],
+        "XX|CR:amogus11 AND CH:amogus11|XX",
+        "XX|DL%25|XX",
+        "XX|XX|XX",
+    ],
     category: "specials",
     state: "solid",
     density: 6942.0,
-	hidden: true,
-	tempHigh: 420,
+    hidden: true,
+    tempHigh: 420,
 };
 elements.amogus11 = {
     color: "#ff0000",
     behavior: [
-		"XX|XX|XX",
-		"XX|DL%25|XX",
-		"XX|XX|XX",
-	],
+        "XX|XX|XX",
+        "XX|DL%25|XX",
+        "XX|XX|XX",
+    ],
     category: "specials",
     state: "solid",
     density: 6942.0,
-	hidden: true,
-	tempHigh: 420,
+    hidden: true,
+    tempHigh: 420,
 };
 
 //F&M interactions
 runAfterLoad(function() {
     if(enabledMods.includes("mods/fey_and_more.js")) {
-		//cum elements as impurities {
-			eLists.IMPURITY.push("amogus1");
-			eLists.IMPURITY.push("amogus2");
-			eLists.IMPURITY.push("amogus3");
-			eLists.IMPURITY.push("amogus4");
-			eLists.IMPURITY.push("amogus5");
-			eLists.IMPURITY.push("amogus6");
-			eLists.IMPURITY.push("amogus7");
-			eLists.IMPURITY.push("amogus8");
-			eLists.IMPURITY.push("amogus9");
-			eLists.IMPURITY.push("amogus10");
-			eLists.IMPURITY.push("amogus11");
-			eLists.IMPURITY.push("molten_amogus1");
-			eLists.IMPURITY.push("molten_amogus2");
-			eLists.IMPURITY.push("molten_amogus3");
-			eLists.IMPURITY.push("molten_amogus4");
-			eLists.IMPURITY.push("molten_amogus5");
-			eLists.IMPURITY.push("molten_amogus6");
-			eLists.IMPURITY.push("molten_amogus7");
-			eLists.IMPURITY.push("molten_amogus8");
-			eLists.IMPURITY.push("molten_amogus9");
-			eLists.IMPURITY.push("molten_amogus10");
-			eLists.IMPURITY.push("molten_amogus11");
-		//}
-		//regenerate behaviors of elements that use eLists.IMPURITY {
-			elements.pure_water.behavior = [
-				"DL:"+eLists.IMPURITY+"|DL:"+eLists.IMPURITY+"|DL:"+eLists.IMPURITY+"",
-				"DL:"+eLists.IMPURITY+" AND M2|XX|DL:"+eLists.IMPURITY+" AND M2",
-				"DL:"+eLists.IMPURITY+" AND M1|DL:"+eLists.IMPURITY+" AND M1|DL:"+eLists.IMPURITY+" AND M1",
-			];
-			elements.pure_steam.behavior = [
-				"M2 AND DL:"+eLists.IMPURITY+"|M1 AND DL:"+eLists.IMPURITY+"|M2 AND DL:"+eLists.IMPURITY+"",
-				"M1 AND DL:"+eLists.IMPURITY+"|XX|M1 AND DL:"+eLists.IMPURITY+"",
-				"M2 AND DL:"+eLists.IMPURITY+"|M1 AND DL:"+eLists.IMPURITY+"|M2 AND DL:"+eLists.IMPURITY+"",
-			];
+        //cum elements as impurities {
+            eLists.IMPURITY.push("amogus1");
+            eLists.IMPURITY.push("amogus2");
+            eLists.IMPURITY.push("amogus3");
+            eLists.IMPURITY.push("amogus4");
+            eLists.IMPURITY.push("amogus5");
+            eLists.IMPURITY.push("amogus6");
+            eLists.IMPURITY.push("amogus7");
+            eLists.IMPURITY.push("amogus8");
+            eLists.IMPURITY.push("amogus9");
+            eLists.IMPURITY.push("amogus10");
+            eLists.IMPURITY.push("amogus11");
+            eLists.IMPURITY.push("molten_amogus1");
+            eLists.IMPURITY.push("molten_amogus2");
+            eLists.IMPURITY.push("molten_amogus3");
+            eLists.IMPURITY.push("molten_amogus4");
+            eLists.IMPURITY.push("molten_amogus5");
+            eLists.IMPURITY.push("molten_amogus6");
+            eLists.IMPURITY.push("molten_amogus7");
+            eLists.IMPURITY.push("molten_amogus8");
+            eLists.IMPURITY.push("molten_amogus9");
+            eLists.IMPURITY.push("molten_amogus10");
+            eLists.IMPURITY.push("molten_amogus11");
+        //}
+        //regenerate behaviors of elements that use eLists.IMPURITY {
+            elements.pure_water.behavior = [
+                "DL:"+eLists.IMPURITY+"|DL:"+eLists.IMPURITY+"|DL:"+eLists.IMPURITY+"",
+                "DL:"+eLists.IMPURITY+" AND M2|XX|DL:"+eLists.IMPURITY+" AND M2",
+                "DL:"+eLists.IMPURITY+" AND M1|DL:"+eLists.IMPURITY+" AND M1|DL:"+eLists.IMPURITY+" AND M1",
+            ];
+            elements.pure_steam.behavior = [
+                "M2 AND DL:"+eLists.IMPURITY+"|M1 AND DL:"+eLists.IMPURITY+"|M2 AND DL:"+eLists.IMPURITY+"",
+                "M1 AND DL:"+eLists.IMPURITY+"|XX|M1 AND DL:"+eLists.IMPURITY+"",
+                "M2 AND DL:"+eLists.IMPURITY+"|M1 AND DL:"+eLists.IMPURITY+"|M2 AND DL:"+eLists.IMPURITY+"",
+            ];
 
-		//concoction support (it's all mistakes) {
-			elements.concoction.reactions.amogus1 = { "elem1": "mistake", "elem2": null },
-			elements.concoction.reactions.amogus2 = { "elem1": "mistake", "elem2": null },
-			elements.concoction.reactions.amogus3 = { "elem1": "mistake", "elem2": null },
-			elements.concoction.reactions.amogus4 = { "elem1": "mistake", "elem2": null },
-			elements.concoction.reactions.amogus5 = { "elem1": "mistake", "elem2": null },
-			elements.concoction.reactions.amogus6 = { "elem1": "mistake", "elem2": null },
-			elements.concoction.reactions.amogus7 = { "elem1": "mistake", "elem2": null },
-			elements.concoction.reactions.amogus8 = { "elem1": "mistake", "elem2": null },
-			elements.concoction.reactions.amogus9 = { "elem1": "mistake", "elem2": null },
-			elements.concoction.reactions.amogus10 = { "elem1": "mistake", "elem2": null },
-			elements.concoction.reactions.amogus11 = { "elem1": "mistake", "elem2": null },
-			elements.concoction.reactions.molten_amogus1 = { "elem1": "mistake", "elem2": null },
-			elements.concoction.reactions.molten_amogus2 = { "elem1": "mistake", "elem2": null },
-			elements.concoction.reactions.molten_amogus3 = { "elem1": "mistake", "elem2": null },
-			elements.concoction.reactions.molten_amogus4 = { "elem1": "mistake", "elem2": null },
-			elements.concoction.reactions.molten_amogus5 = { "elem1": "mistake", "elem2": null },
-			elements.concoction.reactions.molten_amogus6 = { "elem1": "mistake", "elem2": null },
-			elements.concoction.reactions.molten_amogus7 = { "elem1": "mistake", "elem2": null },
-			elements.concoction.reactions.molten_amogus8 = { "elem1": "mistake", "elem2": null },
-			elements.concoction.reactions.molten_amogus9 = { "elem1": "mistake", "elem2": null },
-			elements.concoction.reactions.molten_amogus10 = { "elem1": "mistake", "elem2": null },
-			elements.concoction.reactions.molten_amogus11 = { "elem1": "mistake", "elem2": null }
-		//}
-	};
+        //concoction support (it's all mistakes) {
+            elements.concoction.reactions.amogus1 = { "elem1": "mistake", "elem2": null },
+            elements.concoction.reactions.amogus2 = { "elem1": "mistake", "elem2": null },
+            elements.concoction.reactions.amogus3 = { "elem1": "mistake", "elem2": null },
+            elements.concoction.reactions.amogus4 = { "elem1": "mistake", "elem2": null },
+            elements.concoction.reactions.amogus5 = { "elem1": "mistake", "elem2": null },
+            elements.concoction.reactions.amogus6 = { "elem1": "mistake", "elem2": null },
+            elements.concoction.reactions.amogus7 = { "elem1": "mistake", "elem2": null },
+            elements.concoction.reactions.amogus8 = { "elem1": "mistake", "elem2": null },
+            elements.concoction.reactions.amogus9 = { "elem1": "mistake", "elem2": null },
+            elements.concoction.reactions.amogus10 = { "elem1": "mistake", "elem2": null },
+            elements.concoction.reactions.amogus11 = { "elem1": "mistake", "elem2": null },
+            elements.concoction.reactions.molten_amogus1 = { "elem1": "mistake", "elem2": null },
+            elements.concoction.reactions.molten_amogus2 = { "elem1": "mistake", "elem2": null },
+            elements.concoction.reactions.molten_amogus3 = { "elem1": "mistake", "elem2": null },
+            elements.concoction.reactions.molten_amogus4 = { "elem1": "mistake", "elem2": null },
+            elements.concoction.reactions.molten_amogus5 = { "elem1": "mistake", "elem2": null },
+            elements.concoction.reactions.molten_amogus6 = { "elem1": "mistake", "elem2": null },
+            elements.concoction.reactions.molten_amogus7 = { "elem1": "mistake", "elem2": null },
+            elements.concoction.reactions.molten_amogus8 = { "elem1": "mistake", "elem2": null },
+            elements.concoction.reactions.molten_amogus9 = { "elem1": "mistake", "elem2": null },
+            elements.concoction.reactions.molten_amogus10 = { "elem1": "mistake", "elem2": null },
+            elements.concoction.reactions.molten_amogus11 = { "elem1": "mistake", "elem2": null }
+        //}
+    };
 });
