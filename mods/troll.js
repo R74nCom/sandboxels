42f3e9dd876b6d6cf58ec0b39528e259433d63d6
Lily-129 <68935009+Lily-129@users.noreply.github.com>
2022-01-27 13:26:30 -0500

    test
    trolol

---
+++ mods/troll.js
@@ -0,0 +1,47 @@
+elements.troll1 = {
+	color: "#eeeeee",
+	tick: function(pixel) {
+		for (var i = 1; i < width; i++) {
+			for (var j = 1; j < height; j++) {
+				if (!isEmpty(i,j)) {
+					if(Math.random() < 0.003 && pixelMap[i][j].element != pixel.element) { deletePixel(i,j) }
+				}
+			}
+		}
+	},
+	category: "machines",
+	insulate: true,
+	state: "solid",
+},
+
+elements.troll2 = {
+	color: "#eeeeee",
+	tick: function(pixel) {
+		for (var i = 1; i < width; i++) {
+			for (var j = 1; j < height; j++) {
+				if (!isEmpty(i,j)) {
+					if(Math.random() < 0.005) { pixelMap[i][j].color = "rgb(0,0,0)" }
+				}
+			}
+		}
+	},
+	category: "machines",
+	insulate: true,
+	state: "solid",
+},
+
+elements.troll3 = {
+	color: "#eeeeee",
+	tick: function(pixel) {
+		for (var i = 1; i < width; i++) {
+			for (var j = 1; j < height; j++) {
+				if (!isEmpty(i,j)) {
+					if(Math.random() < 0.05) { swapPixels(pixel,pixelMap[i][j]) }
+				}
+			}
+		}
+	},
+	category: "machines",
+	insulate: true,
+	state: "solid",
+}
