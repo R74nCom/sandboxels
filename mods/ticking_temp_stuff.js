0886b8c84f01ac17ec5b052ea1e28bd938cc1a46
Lily-129 <68935009+Lily-129@users.noreply.github.com>
2022-01-28 12:21:16 -0500

    re-added tier 4

--- mods/ticking_temp_stuff.js
+++ mods/ticking_temp_stuff.js
@@ -161,4 +161,133 @@ elements.agw = { //adjustable global warmer
 	insulate: true,
 	state: "solid",
 	hidden: true,
-}
+},
+
+elements.superheater4 = {
+	color: "#ff7f00",
+	uwu: 0,
+	tick: function(pixel) {
+		tempInc = 50
+		pixel.uwu = 0
+		range = 10
+		for (let i = -8; i < 9; i++) {
+			for (let j = -8; j < 9; j++) {
+				if (!isEmpty(pixel.x+j,pixel.y+i) && !outOfBounds(pixel.x+j,pixel.y+i)) {
+					if (pixelMap[pixel.x+j][pixel.y+i].element == pixel.element) {
+						pixel.uwu += 1
+					}
+				}
+			}
+		}
+		pixel.uwu -= 1
+		if(pixel.uwu == undefined || pixel.uwu == null || isNaN(pixel.uwu)) {
+			pixel.uwu = 0
+		} else {
+			tempInc += (pixel.uwu*15)
+			range += Math.floor((Math.sqrt(pixel.uwu+1))**1.2)
+		}
+		for (let i = (-1*range); i < (range + 1); i++) {
+			for (let j = (-1*range); j < (range + 1); j++) {
+				if (!isEmpty(pixel.x+j,pixel.y+i) && !outOfBounds(pixel.x+j,pixel.y+i)) {
+					pixelMap[pixel.x+j][pixel.y+i].temp += tempInc
+				}
+			}
+		}
+	},
+	category:"machines",
+	insulate:true,
+	state: "solid",
+},
+
+elements.supercooler4 = {
+	color: "#007fff",
+	uwu: 0,
+	tick: function(pixel) {
+		tempDec = 50
+		pixel.uwu = 0
+		range = 10
+		for (let i = -8; i < 9; i++) {
+			for (let j = -8; j < 9; j++) {
+				if (!isEmpty(pixel.x+j,pixel.y+i) && !outOfBounds(pixel.x+j,pixel.y+i)) {
+					if (pixelMap[pixel.x+j][pixel.y+i].element == pixel.element) {
+						pixel.uwu += 1
+					}
+				}
+			}
+		}
+		pixel.uwu -= 1
+		if(pixel.uwu == undefined || pixel.uwu == null || isNaN(pixel.uwu)) {
+			pixel.uwu = 0
+		} else {
+			tempDec += (pixel.uwu*15)
+			range += Math.floor((Math.sqrt(pixel.uwu+1))**1.2)
+		}
+		for (let i = (-1*range); i < (range + 1); i++) {
+			for (let j = (-1*range); j < (range + 1); j++) {
+				if (!isEmpty(pixel.x+j,pixel.y+i) && !outOfBounds(pixel.x+j,pixel.y+i)) {
+					(pixelMap[pixel.x+j][pixel.y+i].temp < (-273 + tempDec)) ? pixelMap[pixel.x+j][pixel.y+i].temp = -273 : pixelMap[pixel.x+j][pixel.y+i].temp -= tempDec
+				}
+			}
+		}
+	},
+	category:"machines",
+	insulate:true,
+	state: "solid",
+},
+
+elements.superwarmer4 = {
+	color: "#7fff7f",
+	uwu: 0,
+	tick: function(pixel) {
+		pixel.uwu = 0
+		range = 10
+		for (let i = -8; i < 9; i++) {
+			for (let j = -8; j < 9; j++) {
+				if (!isEmpty(pixel.x+j,pixel.y+i) && !outOfBounds(pixel.x+j,pixel.y+i)) {
+					if (pixelMap[pixel.x+j][pixel.y+i].element == pixel.element) {
+						pixel.uwu += 1
+					}
+				}
+			}
+		}
+		pixel.uwu -= 1
+		if(pixel.uwu == undefined || pixel.uwu == null || isNaN(pixel.uwu)) {
+			pixel.uwu = 0
+		} else {
+			range += Math.floor((Math.sqrt(pixel.uwu+1))**1.2)
+		}
+		for (let i = (-1*range); i < (range + 1); i++) {
+			for (let j = (-1*range); j < (range + 1); j++) {
+				if (!isEmpty(pixel.x+j,pixel.y+i) && !outOfBounds(pixel.x+j,pixel.y+i)) {
+					pixelMap[pixel.x+j][pixel.y+i].temp = 20
+				}
+			}
+		}
+	},
+	category:"machines",
+	insulate:true,
+	state: "solid",
+},
+
+elements.tc = { //temperature checker
+	color: ["#000000","#000000"],
+	tick: function(pixel) {
+		if(pixel.temp < -255) {
+			pixel.color = "rgb(0,0,255)"
+		} else if(pixel.temp >= -255 && pixel.temp < 0) {
+			pixel.color = "rgb(0,0," + Math.abs(pixel.temp) + ")"
+		} else if(pixel.temp <= 255) {
+			pixel.color = "rgb(" + pixel.temp % 256 + ",0,0)"
+		} else if(pixel.temp <= 65535) {
+			pixel.color = "rgb(255," + Math.floor(pixel.temp / 256) + ",0)"
+		} else if(pixel.temp <= 16777215) {
+			pixel.color = "rgb(255,255," + Math.floor(pixel.temp / 65536) + ")"
+		} else {
+			pixel.color = "rgb(255,255,255)"
+		}
+	},
+	category:"machines",
+	insulate:true,
+	state: "solid",
+},
+
