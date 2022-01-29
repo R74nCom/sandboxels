c01c09121a29a943fec9f69fa7292ef96c0eb598
Lily-129 <68935009+Lily-129@users.noreply.github.com>
2022-01-29 18:14:34 -0500

    more hrt chemicals

--- mods/randomness.js
+++ mods/randomness.js
@@ -303,7 +303,7 @@ elements.lb1 = {
 	state: "liquid",
 	density: 2725,
 	hidden: true,
-}
+},
 
 elements.unnamed_substance_bomb = {
 	name: "unnamed bomb",
@@ -343,9 +343,15 @@ elements.cluster_nuke = {
 	density: 1500,
 },
 
+//hormones
+
+	//estrogens
+
 elements.estradiol = {
-	color: "#eeeeee",
-		behavior: behaviors.POWDER,
+	color: "#f2fcee", //it absorbs shorter wavelength UV than testosterone and I am treating this like absorbing violet for convenience
+					  //https://www.researchgate.net/publication/226065469_Optical_Properties_of_Two_Types_of_Sex_Hormones_of_the_Cyclopentenephenanthrene_Series
+					  //http://depts.washington.edu/cmditr/modules/lum/color.html
+	behavior: behaviors.POWDER,
 	state: "solid",
 	category: "solids",
 	density: 1200,
@@ -359,7 +365,7 @@ elements.molten_estradiol = {
 },
 
 elements.vaporized_estradiol = {
-	color: ["#ffbf60","#ffdc60","#ff9d60"],
+	color: ["#ffbf60","#ffdc60","#ff9d60"], //hormone gas wouldn't glow that brightly at these temperatures but just ignore that
 	behavior: behaviors.GAS,
 	state: "gas",
 	category: "gases",
@@ -368,6 +374,277 @@ elements.vaporized_estradiol = {
 	tempLow: 446,
 	stateLow: "molten_estradiol",
 },
+
+	//progestogens
+
+elements.progesterone = {
+	color: "#f7eefc", //slightly different? from testosterone but exaggerated
+					  //https://downloads.hindawi.com/journals/ijps/2017/9603140.pdf
+					  //these hormones all absorb in the uv region anyway so they would all look white to us
+	behavior: behaviors.POWDER,
+	state: "solid",
+	category: "solids",
+	density: 1100,
+	tempHigh: 121,
+	category: "powders",
+},
+
+elements.molten_progesterone = {
+	tempHigh: 447,
+	stateHigh: "vaporized_progesterone",
+},
+
+elements.vaporized_progesterone = {
+	color: ["#ffbf60","#ffdc60","#ff9d60"],
+	behavior: behaviors.GAS,
+	state: "gas",
+	category: "gases",
+	hidden: true,
+	density: 891,
+	tempLow: 447,
+	stateLow: "molten_progesterone",
+}
+
+	//androgens
+
+elements.testosterone = {
+	color: "#f7eef7", //it absorbs longer wavelength UV than estradiol and I am treating this like absorbing green for convenience
+	behavior: behaviors.POWDER,
+	state: "solid",
+	category: "solids",
+	density: 1100,
+	tempHigh: 155,
+	category: "powders",
+},
+
+elements.molten_testosterone = {
+	tempHigh: 433,
+	stateHigh: "vaporized_testosterone",
+},
+
+elements.vaporized_testosterone = {
+	color: ["#ffbf60","#ffdc60","#ff9d60"],
+	behavior: behaviors.GAS,
+	state: "gas",
+	category: "gases",
+	hidden: true,
+	density: 891,
+	tempLow: 433,
+	stateLow: "molten_testosterone",
+},
+
+//other
+
+	//anti-androgens
+
+		//CPA
+
+elements.cyproterone_acetate = {
+	color: "#efeef8", //it absorbs far longer uv than the others, which i am rendering as red absorption
+					  //https://www.researchgate.net/figure/UV-spectrum-for-drospirenone-cyproterone-acetate-desogestrel-and-ethinyl-estradiol-at-1_fig1_315746083
+					  //i didn't really expect to find a spectrum for this
+	behavior: behaviors.POWDER,
+	state: "solid",
+	category: "solids",
+	density: 1068,
+	tempHigh: 200,
+	category: "powders",
+},
+
+/*	> Hazardous decomposition products:
+	> Hydrogen chloride (HCl)
+	> Carbon monoxide and carbon dioxide
+	> Hydrogen
+	
+	> https://cdn.caymanchem.com/cdn/msds/16622m.pdf
+	
+	so many interesting effects i can't add
+*/
+elements.molten_cyproterone_acetate = {
+	tempHigh: 569,
+	stateHigh: "vaporized_cyproterone_acetate",
+},
+
+elements.vaporized_cyproterone_acetate = {
+	color: ["#ffbf60","#ffdc60","#ff9d60"],
+	behavior: behaviors.GAS,
+	state: "gas",
+	category: "gases",
+	hidden: true,
+	density: 865,
+	tempLow: 569,
+	stateLow: "molten_cyproterone_acetate",
+},
+
+		//spironolactone
+
+elements.spironolactone = {
+	color: "#f7eef1", //UV absorbance peak wavelength is slightly shorter than that of testosterone
+					  //https://www.researchgate.net/publication/348592381_Quantification_of_Spironolactone_by_first_and_second_order_UV_Derivative_Spectrophotometry_in_bulk_and_tablet_dosage_form/link/6006b3cf299bf14088a649bd/download
+	behavior: behaviors.POWDER,
+	state: "solid",
+	category: "solids",
+	density: 1200,
+	tempHigh: 207,
+	category: "powders",
+},
+
+elements.molten_spironolactone = {
+	tempHigh: 597,
+	stateHigh: "vaporized_spironolactone",
+	/*should have more decomps
+	https://sci-hub.se/https://link.springer.com/article/10.1007/BF01979243
+	> The TG-DTG curves of spironolactone in Fig. 7 demonstrate that the compound is thermally stable up to 200*C, and that its thermal decomposition occurs between 200 and 620*C. Four consecutive steps are observed in the TG-DTG curves. The first step, up to 260*C is ascribed to the elimination of the substituent group, SCOCH_{3} (TG= 19.59%, Calc. = 19.33%). The second step (260-370*C) and the third and fourth steps (370-700*C) involve losses of 42.93% and 37.48%, respectively, but do not permit a suggestion as to which parts of the compound are eliminated in each step. */
+},
+
+elements.vaporized_spironolactone = {
+	color: ["#ffbf60","#ffdc60","#ff9d60"],
+	behavior: behaviors.GAS,
+	state: "gas",
+	category: "gases",
+	hidden: true,
+	density: 972,
+	tempLow: 597,
+	stateLow: "molten_spironolactone",
+},
+
+		//finasteride
+
+elements.finasteride = {
+	color: "#fcfcf1", //UV absorbance peak wavelength is even shorter than that of estradiol
+					  //https://www.researchgate.net/publication/312317200
+	behavior: behaviors.POWDER,
+	state: "solid",
+	category: "solids",
+	density: 1100,
+	tempHigh: 253,
+	category: "powders",
+},
+
+elements.molten_finasteride = {
+	tempHigh: 577,
+	stateHigh: "vaporized_finasteride",
+},
+
+elements.vaporized_finasteride = {
+	color: ["#ffbf60","#ffdc60","#ff9d60"],
+	behavior: behaviors.GAS,
+	state: "gas",
+	category: "gases",
+	hidden: true,
+	density: 891,
+	tempLow: 577,
+	stateLow: "molten_finasteride",
+},
+
+		//dutasteride
+
+elements.dutasteride = {
+	color: "#fbf6ee", //High UV absorbances around the peak wavelengths of both estradiol and testosterone
+					  //https://sphinxsai.com/sphinxsaivol_2no.1/pharmtech_vol_2no.1/PharmTech_Vol_2No.1PDF/PT=18%20(113-117).pdf
+	behavior: behaviors.POWDER,
+	state: "solid",
+	category: "solids",
+	density: 1303, //https://www.chemicalbook.com/ChemicalProductProperty_EN_CB3254628.htm
+	tempHigh: 243,
+	category: "powders",
+},
+
+elements.molten_dutasteride = {
+	tempHigh: 620, //http://www.chemspider.com/Chemical-Structure.5293502.html
+	stateHigh: "vaporized_dutasteride",
+},
+
+elements.vaporized_dutasteride = {
+	color: ["#ffbf60","#ffdc60","#ff9d60"],
+	behavior: behaviors.GAS,
+	state: "gas",
+	category: "gases",
+	hidden: true,
+	density: 1055,
+	tempLow: 620,
+	stateLow: "molten_dutasteride",
+},
+
+		//bicalutamide
+
+elements.bicalutamide = {
+	color: "#f4fcee", //peaks at 200-220 and at 270
+					  //i am probably mapping uv to visible wrong and misreading color.html
+					  //https://www.researchgate.net/publication/257679318
+	behavior: behaviors.POWDER,
+	state: "solid",
+	category: "solids",
+	density: 1520, //https://www.chemicalbook.com/ProductMSDSDetailCB7457827_EN.htm
+	tempHigh: 192,
+	category: "powders",
+},
+
+elements.molten_bicalutamide = {
+	tempHigh: 659,
+	stateHigh: "vaporized_bicalutamide",
+},
+
+elements.vaporized_bicalutamide = {
+	color: ["#ffbf60","#ffdc60","#ff9d60"],
+	behavior: behaviors.GAS,
+	state: "gas",
+	category: "gases",
+	hidden: true,
+	density: 1231,
+	tempLow: 659,
+	stateLow: "molten_bicalutamide",
+},
+
+	//puberty blockers
+
+elements.leuprolide = {
+	color: "#f5eefb", //http://dspace.hmlibrary.ac.in:8080/jspui/bitstream/123456789/1143/11/11_Chapter%203.pdf
+	behavior: behaviors.POWDER,
+	state: "solid",
+	category: "solids",
+	density: 1440, //https://www.chemicalbook.com/ProductMSDSDetailCB7457827_EN.htm
+	tempHigh: 150,
+	category: "powders",
+},
+
+elements.molten_leuprolide = {
+	tempHigh: 1720, //https://web.archive.org/web/20210512074205/http://www.shreejipharmainternational.com/leuprolide-acetate-1177796.html
+	stateHigh: "vaporized_leuprolide",
+},
+
+elements.vaporized_leuprolide = {
+	color: ["#ffbf60","#ffdc60","#ff9d60"],
+	behavior: behaviors.GAS,
+	state: "gas",
+	category: "gases",
+	hidden: true,
+	density: 1166,
+	tempLow: 1720,
+	stateLow: "molten_leuprolide",
+},
+
+	//histrelin
+
+elements.histrelin = {
+	color: "#f8f5ee", //no spectrum available
+	behavior: behaviors.POWDER,
+	state: "solid",
+	category: "solids",
+	density: 1500, //https://www.chemicalbook.com/ProductMSDSDetailCB7457827_EN.htm
+	tempHigh: 1800, //https://www.chemsrc.com/en/cas/76712-82-8_1042020.html
+	category: "powders",
+},
+
+elements.molten_histrelin = {
+	color: ["#ffbf60","#ffdc60","#ff9d60"],
+	behavior: behaviors.GAS,
+	state: "gas",
+	category: "gases",
+	hidden: true,
+	tempLow: 1800,
+	stateLow: "histrelin",
+}
 
 runAfterLoad(function() {
 	elements.tungstensteel.behavior = [
@@ -380,5 +657,8 @@ runAfterLoad(function() {
         "DL:"+eLists.FAIRY+" AND M2|XX|DL:"+eLists.FAIRY+" AND M2",
         "M1|DL:"+eLists.FAIRY+"|M1",
     ]
+	elements.vaporized_histrelin = elements.molten_histrelin
+	delete elements.molten_histrelin
+	elements.histrelin.stateHigh = "vaporized_histrelin"
+	elements.vaporized_histrelin.stateLow = "histrelin"
 });
-//dummy edit?
