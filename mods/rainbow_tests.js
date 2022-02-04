e02cead0e2e1efa34bb6a9ea9f07b11ec3446fa5
Lily-129 <68935009+Lily-129@users.noreply.github.com>
2022-02-03 22:41:05 -0500

    idk

--- mods/rainbow_tests.js
+++ mods/rainbow_tests.js
@@ -11,6 +11,43 @@ elements.rainbow_alt_test = {
 	category: "idk",
 }
 
+elements.red_test = {
+	color: ["#ff0000","#200000","#ff0000","#200000"],
+	tick: function(pixel) {
+		var t = pixelTicks*2.5+pixel.x+pixel.y;
+		var r = Math.floor(255*(1-Math.cos(t*Math.PI/18)));
+		pixel.color = "rgb("+Math.ceil((r*(7/8))+32)+","+0+","+0+")";
+		doHeat(pixel);
+	},
+	category: "idk",
+}
+
+elements.rainbow_alt_test_small = {
+	color: ["#ffaacc","#ffaacc","#aaccff","#aaccff","#ffffbb","#ffffbb"],
+	tick: function(pixel) {
+		var t = pixelTicks*3+pixel.x+pixel.y;
+		var r = Math.floor(255*(1-Math.cos(t*Math.PI/60)));
+		var g = Math.floor(255*(1-Math.cos(t*Math.PI/60+2*Math.PI/3)));
+		var b = Math.floor(255*(1-Math.cos(t*Math.PI/60+4*Math.PI/3)));
+		pixel.color = "rgb("+Math.ceil((r/2)+127)+","+Math.ceil((g/2)+127)+","+Math.ceil((b/2)+127)+")";
+		doHeat(pixel);
+	},
+	category: "idk",
+}
+
+elements.rainbow_alt_test_x_d_t = {
+	color: ["#ffaacc","#ffaacc","#aaccff","#aaccff","#ffffbb","#ffffbb"],
+	tick: function(pixel) {
+		var t = pixelTicks*3+pixel.x+pixel.y;
+		var r = Math.floor(255*(1-Math.cos(t*Math.PI/(pixelTicks/pixel.x))));
+		var g = Math.floor(255*(1-Math.cos(t*Math.PI/(pixelTicks/pixel.x)+2*Math.PI/3)));
+		var b = Math.floor(255*(1-Math.cos(t*Math.PI/(pixelTicks/pixel.x)+4*Math.PI/3)));
+		pixel.color = "rgb("+Math.ceil((r/2)+127)+","+Math.ceil((g/2)+127)+","+Math.ceil((b/2)+127)+")";
+		doHeat(pixel);
+	},
+	category: "idk",
+}
+
 elements.rainbow_alt_test_pixel_x = {
 	color: ["#ffaacc","#ffaacc","#aaccff","#aaccff","#ffffbb","#ffffbb"],
 	tick: function(pixel) {
@@ -245,6 +282,19 @@ elements.rainbow_alt_test_pixel_d_m_x = {
 	category: "idk",
 }
 
+elements.rainbow_alt_test_pixel_d_t_x = {
+	color: ["#ffaacc","#ffaacc","#aaccff","#aaccff","#ffffbb","#ffffbb"],
+	tick: function(pixel) {
+		var t = pixelTicks*3+pixel.x+pixel.y;
+		var r = Math.floor(255*(1-Math.cos(t*Math.PI/(Math.sqrt(pixel.x**2+pixel.y**2))*pixel.x)));
+		var g = Math.floor(255*(1-Math.cos(t*Math.PI/(Math.sqrt(pixel.x**2+pixel.y**2))*pixel.x+2*Math.PI/3)));
+		var b = Math.floor(255*(1-Math.cos(t*Math.PI/(Math.sqrt(pixel.x**2+pixel.y**2))*pixel.x+4*Math.PI/3)));
+		pixel.color = "rgb("+Math.ceil((r/2)+127)+","+Math.ceil((g/2)+127)+","+Math.ceil((b/2)+127)+")";
+		doHeat(pixel);
+	},
+	category: "idk",
+}
+
 elements.rainbow_alt_test_pixel_x_d_d = {
 	color: ["#ffaacc","#ffaacc","#aaccff","#aaccff","#ffffbb","#ffffbb"],
 	tick: function(pixel) {
@@ -252,6 +302,44 @@ elements.rainbow_alt_test_pixel_x_d_d = {
 		var r = Math.floor(255*(1-Math.cos(t*Math.PI/(pixel.x/(Math.sqrt(pixel.x**2+pixel.y**2))))));
 		var g = Math.floor(255*(1-Math.cos(t*Math.PI/(pixel.x/(Math.sqrt(pixel.x**2+pixel.y**2)))+2*Math.PI/3)));
 		var b = Math.floor(255*(1-Math.cos(t*Math.PI/(pixel.x/(Math.sqrt(pixel.x**2+pixel.y**2)))+4*Math.PI/3)));
+		pixel.color = "rgb("+Math.ceil((r/2)+127)+","+Math.ceil((g/2)+127)+","+Math.ceil((b/2)+127)+")";
+		doHeat(pixel);
+	},
+	category: "idk",
+}
+
+elements.rainbow_alt_test_pixel_x_mod_y = {
+	color: ["#ffaacc","#ffaacc","#aaccff","#aaccff","#ffffbb","#ffffbb"],
+	tick: function(pixel) {
+		var t = pixelTicks*3+pixel.x+pixel.y;
+		var r = Math.floor(255*(1-Math.cos(t*Math.PI/(pixel.x%pixel.y))));
+		var g = Math.floor(255*(1-Math.cos(t*Math.PI/(pixel.x%pixel.y)+2*Math.PI/3)));
+		var b = Math.floor(255*(1-Math.cos(t*Math.PI/(pixel.x%pixel.y)+4*Math.PI/3)));
+		pixel.color = "rgb("+Math.ceil((r/2)+127)+","+Math.ceil((g/2)+127)+","+Math.ceil((b/2)+127)+")";
+		doHeat(pixel);
+	},
+	category: "idk",
+}
+
+elements.rainbow_alt_test_pixel_d_mod_x = {
+	color: ["#ffaacc","#ffaacc","#aaccff","#aaccff","#ffffbb","#ffffbb"],
+	tick: function(pixel) {
+		var t = pixelTicks*3+pixel.x+pixel.y;
+		var r = Math.floor(255*(1-Math.cos(t*Math.PI/((Math.sqrt(pixel.x**2+pixel.y**2))%pixel.x))));
+		var g = Math.floor(255*(1-Math.cos(t*Math.PI/((Math.sqrt(pixel.x**2+pixel.y**2))%pixel.x)+2*Math.PI/3)));
+		var b = Math.floor(255*(1-Math.cos(t*Math.PI/((Math.sqrt(pixel.x**2+pixel.y**2))%pixel.x)+4*Math.PI/3)));
+		pixel.color = "rgb("+Math.ceil((r/2)+127)+","+Math.ceil((g/2)+127)+","+Math.ceil((b/2)+127)+")";
+		doHeat(pixel);
+	},
+	category: "idk",
+}
+elements.rainbow_alt_test_pixel_d_mod_y = {
+	color: ["#ffaacc","#ffaacc","#aaccff","#aaccff","#ffffbb","#ffffbb"],
+	tick: function(pixel) {
+		var t = pixelTicks*3+pixel.x+pixel.y;
+		var r = Math.floor(255*(1-Math.cos(t*Math.PI/((Math.sqrt(pixel.x**2+pixel.y**2))%pixel.y))));
+		var g = Math.floor(255*(1-Math.cos(t*Math.PI/((Math.sqrt(pixel.x**2+pixel.y**2))%pixel.y)+2*Math.PI/3)));
+		var b = Math.floor(255*(1-Math.cos(t*Math.PI/((Math.sqrt(pixel.x**2+pixel.y**2))%pixel.y)+4*Math.PI/3)));
 		pixel.color = "rgb("+Math.ceil((r/2)+127)+","+Math.ceil((g/2)+127)+","+Math.ceil((b/2)+127)+")";
 		doHeat(pixel);
 	},
