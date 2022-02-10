b368e6e209ca96a2caffce0cc44e2ade25725c68
StellarX20 <97905447+StellarX20@users.noreply.github.com>
2022-02-10 14:18:13 -0500

    Added glowstone, redstone dust, and added molten netherrack.

--- mods/minecraft.js
+++ mods/minecraft.js
@@ -22,5 +22,37 @@ color: ["#8c2a0a", "#783722"],
 behavior: behaviors.WALL,
 category: "minecraft",
 state: "solid",
-tempHigh: 2750
+tempHigh: 2750,
+stateHigh: "molten_netherrack",
+};
+elements.glowstone_dust = {
+color: ["#d9d636", "#fffc63", "#a3a12f", "#e0dd3f"],
+behavior: behaviors.POWDER,
+category: "minecraft",
+state: "solid",
+tempHigh: 2500,
+stateHigh: "molten_glowstone",
+conduct: 0.975,
+density: 1075
+};
+elements.molten_netherrack = {
+name: "Nether Magma",
+color: ["#f7f09c", "#faf9eb", "#ffffff", "#dcf7f7", "#86dbdb", "#1268a6"],
+behavior: behaviors.LIQUID,
+category: "minecraft",
+state: "liquid",
+tempLow: 2750,
+stateLow: "netherrack",
+temp: 3000,
+viscosity: 1000,
+density: 2305
+};
+elements.redstone_dust = {
+color: ["#bf2424", "#f22424", "#a12020"],
+behavior: behaviors.POWDER,
+category: "minecraft",
+state: "solid",
+tempHigh: 1275,
+stateHigh: "liquid_redstone",
+density: 1250
 };
