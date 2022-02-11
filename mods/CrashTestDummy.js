42e3fe60080b8577fafff786bc09a247a730a6c7
StellarX20 <97905447+StellarX20@users.noreply.github.com>
2022-02-11 16:48:17 -0500

    Update CrashTestDummy.js

--- mods/CrashTestDummy.js
+++ mods/CrashTestDummy.js
@@ -1,6 +1,6 @@
 // This new crashtestdummy mod will be more used for code that I'm not sure will actually work
 elements.sandreplacer = {
-    color: "#ff2f2f",
+    color: "#ff80ff",
     tool: function(pixel) {
         pixel.element = "sand"
     },
