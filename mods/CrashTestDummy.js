2612f74043b03dbb50b557e3fed48d0c0ded90ca
StellarX20 <97905447+StellarX20@users.noreply.github.com>
2022-02-11 16:40:45 -0500

    > // This new crashtestdummy mod will be more used for code that I'm not sure will actually work
    Added sand replacer for a test

---
+++ mods/CrashTestDummy.js
@@ -0,0 +1,7 @@
+// This new crashtestdummy mod will be more used for code that I'm not sure will actually work
+elements.sandreplacer = {
+    color: "#ff2f2f",
+    tool: function(pixel) {
+        pixel.type = "sand"
+    },
+    category: "tools",
