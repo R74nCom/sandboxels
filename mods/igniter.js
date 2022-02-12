64c03122de4fbb0d7ed1b6ba6173c82175e49071
Lily-129 <68935009+Lily-129@users.noreply.github.com>
2022-02-11 21:18:04 -0500

    tool that unburns things

--- mods/igniter.js
+++ mods/igniter.js
@@ -7,3 +7,13 @@ elements.igniter = {
 	category: "tools",
 	excludeRandom: true,
 };
+
+elements.extinguisher = {
+	color: "#bad1e3",
+	tool: function(pixel) {
+		if(pixel.burnStart) { delete pixel.burnStart }
+		pixel.burning = false;
+	},
+	category: "tools",
+	excludeRandom: true,
+};
