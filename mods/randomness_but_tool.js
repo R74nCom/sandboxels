41e899929b4173937d753227865ea130058b8712
Lily-129 <68935009+Lily-129@users.noreply.github.com>
2022-02-23 20:07:53 -0500

    mod split

--- mods/randomness_but_tool.js
+++ mods/randomness_but_tool.js
@@ -87,21 +87,3 @@ elements.rg3 = {
 	category: "tools",
 	excludeRandom: true,
 };
-
-elements.daoe = {
-	name: "delete all of element",
-	color: ["#a7a7a7", "#a7a7a7", "#a7a7a7", "#a7a7a7", "#000000", "#000000", "#000000", "#000000"],
-	tool: function(pixel) {
-		for (var i = 1; i < width; i++) {
-			for (var j = 1; j < height; j++) {
-				if (!isEmpty(i,j)) {
-					if(pixelMap[i][j].element == pixel.element) {
-						deletePixel(i,j)
-					}
-				}
-			}
-		}
-	},
-	category: "tools",
-	excludeRandom: true,
-};
