aa20c00b54b9d1721f02fb954c5d97927bb7a027
Lily-129 <68935009+Lily-129@users.noreply.github.com>
2022-02-23 20:08:35 -0500

    delete all of specific element tool

---
+++ mods/delete_all_of_element.js
@@ -0,0 +1,17 @@
+elements.daoe = {
+	name: "delete all of element",
+	color: ["#a7a7a7", "#a7a7a7", "#a7a7a7", "#a7a7a7", "#000000", "#000000", "#000000", "#000000"],
+	tool: function(pixel) {
+		for (var i = 1; i < width; i++) {
+			for (var j = 1; j < height; j++) {
+				if (!isEmpty(i,j)) {
+					if(pixelMap[i][j].element == pixel.element) {
+						deletePixel(i,j)
+					}
+				}
+			}
+		}
+	},
+	category: "tools",
+	excludeRandom: true,
+};
