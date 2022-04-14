35d5a3e804335c5054f62923f16cb4aafb5dfc86
Nubo318 <69615769+Nubo318@users.noreply.github.com>
2022-04-13 21:58:42 -0500

    lactose intolerance and celiac

---
+++ mods/lactose_intolerance_and_celiac.js
@@ -0,0 +1,12 @@
+elements.human.reactions = {
+    "milk": { "elem1":"explosion"},
+    "wheat": { "elem1":"explosion"}
+};
+elements.body.reactions = {
+    "milk": { "elem1":"explosion"},
+    "wheat": { "elem1":"explosion"}
+};
+elements.head.reactions = {
+    "milk": { "elem1":"explosion"},
+    "wheat": { "elem1":"explosion"}
+}
