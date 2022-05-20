9d18b314735fb417e968aee732159cab8a72c810
O-01-67 <68935009+O-01-67@users.noreply.github.com>
2022-05-20 19:18:07 -0400

    simple invisible wall
    element invisible_wall is wall but sets itself to the bg color

---
+++ mods/invisible_wall.js
@@ -0,0 +1,20 @@
+if(!settings) {
+	settings = {}
+}
+
+if(!settings.bg) {
+	settings.bg = "#000000"
+}
+
+elements.invisible_wall = {
+	color: settings.bg,
+	behavior: behaviors.WALL,
+	tick: function(pixel) {
+		var backgroundColor = hexToRGB(settings.bg);
+		var rgbValue = "rgb("+backgroundColor.r+","+backgroundColor.g+","+backgroundColor.b+")";
+		pixel.color = rgbValue;
+	},
+	hardness: 1,
+	category: "special",
+	state: "solid",
+};
