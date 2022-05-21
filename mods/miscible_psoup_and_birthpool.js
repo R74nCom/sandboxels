d3fe08b9242d1a942468fc50b78cd5b7b1c2e63a
O-01-67 <68935009+O-01-67@users.noreply.github.com>
2022-05-20 20:13:45 -0400

    error suppression

--- mods/miscible_psoup_and_birthpool.js
+++ mods/miscible_psoup_and_birthpool.js
@@ -1,11 +1,13 @@
 if(enabledMods.includes("mods/fey_and_more.js")) {
-	elements.birthpool.density = elements.primordial_soup.density;
+	elements.birthpool.density = 1250
+	elements.primordial_soup.density = 1250
 	elements.birthpool.state = "liquid";
 };
 
 runAfterLoad(function() {
 	if(enabledMods.includes("mods/fey_and_more.js")) {
-		elements.birthpool.density = elements.primordial_soup.density;
+		elements.birthpool.density = 1250
+		elements.primordial_soup.density = 1250
 		elements.birthpool.state = "liquid";
 	};
 });
