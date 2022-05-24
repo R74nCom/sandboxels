0b3ea500ca117dde3741ac05da651afec3582e05
O-01-67 <68935009+O-01-67@users.noreply.github.com>
2022-05-24 08:51:44 -0400

    nerf

--- mods/icb.js
+++ mods/icb.js
@@ -3,9 +3,9 @@ for (var i = 3; i <= 15; i++) {
         name: `${i}- cluster bomb`,
         color: "#7d776d",
         behavior: [
-            `XX|EX:10>smoke,smoke,smoke,smoke,smoke,cluster_bomb_${i-1}|XX`,
+            `XX|EX:8>smoke,smoke,smoke,smoke,smoke,cluster_bomb_${i-1}%10|XX`,
             "XX|XX|XX",
-            `M2|M1 AND EX:10>smoke,smoke,smoke,smoke,smoke,cluster_bomb_${i-1}|M2`,
+            `M2|M1 AND EX:8>smoke,smoke,smoke,smoke,smoke,cluster_bomb_${i-1}%10|M2`,
         ],
         category: "weapons",
         state: "solid",
@@ -19,9 +19,9 @@ for (var i = 3; i <= 15; i++) {
 elements.cluster_bomb_2 = {
     color: "#7d776d",
     behavior: [
-        "XX|EX:10>smoke,smoke,smoke,smoke,smoke,cluster_bomb|XX",
+        "XX|EX:8>smoke,smoke,smoke,smoke,smoke,cluster_bomb%10|XX",
         "XX|XX|XX",
-        "M2|M1 AND EX:10>smoke,smoke,smoke,smoke,smoke,cluster_bomb|M2",
+        "M2|M1 AND EX:8>smoke,smoke,smoke,smoke,smoke,cluster_bomb%10|M2",
     ],
     category: "weapons",
     state: "solid",
