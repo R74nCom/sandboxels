65b9187911c4689346fd791ab318340f7c265fee
O-01-67 <68935009+O-01-67@users.noreply.github.com>
2022-06-14 11:03:06 -0400

    places all elements LtR TtB
    use placeAll(number of elements)

---
+++ mods/place_all_elements.js
@@ -0,0 +1,22 @@
+zzzz204689123_array = Object.keys(elements);
+
+    function placeAll(limit=zzzz204689123_array.length) {
+        paused = true;
+        counterLol = 0;
+        loop1:
+        for(i = 1; i < height; i++) {
+            loop2:
+            for(j = 1; j < width; j++) {
+                if(isEmpty(j,i)) {
+                    if(!zzzz204689123_array[counterLol] || counterLol > limit) {
+                        break loop1;
+                    };
+                    createPixel(zzzz204689123_array[counterLol],j,i);
+                    counterLol++;
+                } else {
+                    break loop1;
+                }
+            };
+        };
+        paused = true;
+    };
