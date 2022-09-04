6a4cf5a6e7501df99d26f8b3b6fe377d3de308df
O-01-67 <68935009+O-01-67@users.noreply.github.com>
2022-09-04 16:57:07 -0400

    test (mod order?)

--- mods/miscible_psoup_and_birthpool.js
+++ mods/miscible_psoup_and_birthpool.js
@@ -1,9 +1,3 @@
-if(enabledMods.includes("mods/fey_and_more.js")) {
-	elements.birthpool.density = 1250
-	elements.primordial_soup.density = 1250
-	elements.birthpool.state = "liquid";
-};
-
 runAfterLoad(function() {
 	if(enabledMods.includes("mods/fey_and_more.js")) {
 		elements.birthpool.density = 1250
