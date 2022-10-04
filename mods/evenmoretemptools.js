d0a8353cb92bbcd12ba19a6dea41df5f2b6f6eb1
O-01-67 <68935009+O-01-67@users.noreply.github.com>
2022-10-04 19:14:28 -0400

    room_temp supersedes warm

--- mods/evenmoretemptools.js
+++ mods/evenmoretemptools.js
@@ -1,12 +1,13 @@
 //base syntax by sightnado
-elements.warm = {
+/*elements.warm = {
     color: "#7fff7f",
     tool: function(pixel) {
         pixel.temp = 20;
 		pixelTempCheck(pixel)
     },
     category: "tools",
-};
+};*/
+//warm is redundant due to room_temp
 elements.ultraheat = {
     color: ["#ff0000", "#ffbf4f", "#ff0000", "#ffbf4f", "#ff0000", "#ffbf4f"],
     tool: function(pixel) {
