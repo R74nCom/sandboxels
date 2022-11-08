b90275d8a776891085ba067806409af2463b73c7
Bintushang67 <75445919+Schweeny@users.noreply.github.com>
2022-11-08 10:12:29 -0800

    Update and rename Testruby to Gemstones.js

---
+++ mods/Gemstones.js
@@ -0,0 +1,27 @@
+elements.ruby = {
+  color: ["#ff0000","#f44336","#990000","#ea9999","#f4cccc"], behavior: [
+  category: "powders",
+  state: "solid",
+  density: 265,
+  burn: 1000,
+  burnTime: 300,
+  burnInto: ["carbon dioxide ",null,null],
+},
+elements.sappire = {
+  color: ["#2986cc","#16537e","#0086ff","#9da0f7","#54aeff"], behavior: [
+  category: "powders",
+  state: "solid",
+  density: 265,
+  burn: 1000,
+  burnTime: 300,
+  burnInto: ["carbon dioxide ",null,null],
+},
+elements.emerald = {
+  color: ["#6aa84f","#8fce00","#7cc95a","#8fce00","#c4dabd"], behavior: [
+  category: "powders",
+  state: "solid",
+  density: 265,
+  burn: 1000,
+  burnTime: 300,
+  burnInto: ["carbon dioxide ",null,null],
+}
