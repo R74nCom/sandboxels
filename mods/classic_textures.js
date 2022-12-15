a4c670b26c5759ec83e63c1d5ebeab5bc48f49da
slweeb <91897291+slweeb@users.noreply.github.com>
2022-12-15 10:18:01 -0500

    1.7

---
+++ mods/classic_textures.js
@@ -0,0 +1,85 @@
+elements.dirt.color = [
+    "#9e6b4b",
+    "#9e6b4b",
+    "#9e6b4b",
+    "#9e6b4b",
+    "#9e6b4b",
+    "#9e6b4b",
+    "#9e6b4b",
+    "#9e6b4b",
+    "#9e6b4b",
+    "#9e6b4b",
+    "#9e6b4b",
+    "#9e6b4b",
+    "#9e6b4b",
+    "#9e6b4b",
+    "#a88c7b"
+];
+elements.rotten_meat.color = [
+    "#9ab865",
+    "#b8b165",
+    "#b89765"
+];
+elements.ant.color = "#4a0903";
+elements.worm.color = "#402208";
+elements.fly.color = "#303012";
+elements.firefly.color = [
+    "#310D09",
+    "#310D09",
+    "#d9d950",
+    "#310D09",
+    "#310D09"
+];
+elements.human.color = [
+    "#f5eac6",
+    "#d4c594",
+    "#a89160",
+    "#7a5733",
+    "#523018",
+    "#361e0e"
+];
+elements.body.color = [
+    "#049699",
+    "#638A61"
+];
+elements.head.color = [
+    "#f5eac6",
+    "#d4c594",
+    "#a89160",
+    "#7a6433",
+    "#524018"
+];
+elements.slug.color = [
+    "#997e12",
+    "#403314",
+    "#997e12",
+    "#403314",
+    "#997e12",
+    "#403314",
+    "#997e12",
+    "#403314",
+    "#997e12",
+    "#403314",
+    "#997e12",
+    "#403314",
+    "#124a44"
+];
+elements.light.color = "#ffffa8";
+elements.wheat.color = "#c9bc81";
+elements.permafrost.color = [
+    "#557d65",
+    "#557d79"
+];
+elements.root.color = "#7B6F6B";
+elements.sap.color = [
+    "#B66B18",
+    "#C84805",
+    "#CF6719",
+    "#E49B3A"
+];
+elements.electric.color = "#dddd00";
+elements.epsom_salt.color = [
+    "#f2f2f2",
+    "#e0e0e0"
+];
+elements.flash.color = "#ffffa8"
