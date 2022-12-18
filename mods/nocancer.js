0cc0a80d4f8d392031bec6e563659bf81a2605bb
MollTheCoder <mollthecoder@programmer.net>
2022-12-17 21:28:12 -0500

    Create nocancer.js

---
+++ mods/nocancer.js
@@ -0,0 +1,9 @@
+elements.cancer.behavior = [
+  "XX|XX|XX",
+  "XX|DL|XX",
+  "XX|XX|XX"
+]
+elements.cancer.color = "#000000";
+window.addEventListener("load", ()=>{
+  if(!settings.cheerful)document.getElementById("elementButton-cancer").remove();
+})
