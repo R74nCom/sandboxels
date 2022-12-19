8428d58b0077b17a26c194569e1ed218f4cfcac5
Laetitia (O-01-67) <68935009+O-01-67@users.noreply.github.com>
2022-12-19 16:21:14 -0500

    rework idk
    this code is shit
    at least the prompt is better now and supports multiple elements

--- mods/find.js
+++ mods/find.js
@@ -1,3 +1,122 @@
+//set up a runAfterDraw system while it doesn't warrant a separate mod
+function runAfterDraw(func) {
+	runAfterDrawList.push(func);
+};
+
+runAfterDrawList = [];
+
+runAfterDraw_oldDrawPixels = drawPixels;
+
+runAfterDraw_main = function() {
+	// Loop through runAfterDrawList and run each function
+	for (var i = 0; i < runAfterDrawList.length; i++) {
+		runAfterDrawList[i]();
+	};
+};
+
+runAfterLoad(function() {
+	drawPixels = function(forceTick=false) {
+		runAfterDraw_oldDrawPixels(forceTick);
+		runAfterDraw_main();
+	};
+});
+
+function drawPixels(forceTick=false) {
+	// newCurrentPixels = shuffled currentPixels
+	var newCurrentPixels = currentPixels.slice();
+	var pixelsFirst = [];
+	var pixelsLast = [];
+	if (!paused || forceTick) {
+		shuffleArray(newCurrentPixels);
+	}
+	/*{newCurrentPixels.sort(function(p) { // shuffle the pixels but keep elements[p.element].isGas last
+		return 0.5 - Math.random();
+	})} // shuffle the pixels if not paused*/
+	for (var i = 0; i < newCurrentPixels.length; i++) {
+		pixel = newCurrentPixels[i];
+		//if (pixelMap[pixel.x][pixel.y] == undefined || currentPixels.indexOf(pixel) == -1) {continue}
+		if (pixel.del) {continue}
+		if (!paused || forceTick) {
+			if (elements[pixel.element].tick) { // Run tick function if it exists
+				elements[pixel.element].tick(pixel);
+			}
+			if (pixel.del) {continue}
+			if (elements[pixel.element].behavior) { // Parse behavior if it exists
+				pixelTick(pixel);
+			}
+		};
+		if (elements[pixel.element].isGas) {
+			pixelsLast.push(pixel);
+		}
+		else {
+			pixelsFirst.push(pixel);
+		}
+	}
+	// Draw the current pixels
+	var canvas = document.getElementById("game");
+	var ctx = canvas.getContext("2d");
+	var pixelDrawList = pixelsFirst.concat(pixelsLast);
+	for (var i = 0; i < pixelDrawList.length; i++) {
+		pixel = pixelDrawList[i];
+		if (pixelMap[pixel.x][pixel.y] == undefined) {continue}
+		if (view===null || view===3) {
+			ctx.fillStyle = pixel.color;
+		}
+		else if (view === 2) { // thermal view
+			// set the color to pixel.temp, from hottest at 0 hue to coldest 225 hue, with the minimum being -273, max being 6000
+			var temp = pixel.temp;
+			if (temp < -273) {temp = -273}
+			if (temp > 6000) {temp = 6000}
+			var hue = 225 - (temp/6000)*225;
+			if (hue < 0) {hue = 0}
+			if (hue > 225) {hue = 225}
+			ctx.fillStyle = "hsl("+hue+",100%,50%)";
+		}
+		else if (view === 4) { // smooth view, average of surrounding pixels
+			var colorlist = [];
+			// check adjacent coords on the pixelMap, add the color to the list if the pixel is not empty and the color indexOf "rgb" is not -1
+			for (var j = 0; j < biCoords.length; j++) {
+				var x = pixel.x + biCoords[j][0];
+				var y = pixel.y + biCoords[j][1];
+				if (isEmpty(x,y,true) || elements[pixelMap[x][y].element].state !== elements[pixel.element].state) {continue}
+				var color = pixelMap[x][y].color;
+				if (color.indexOf("rgb") !== -1) {
+					colorlist.push(color.match(/\d+/g));
+				}
+			}
+			if (colorlist.length === 0) {
+				ctx.fillStyle = pixel.color;
+			}
+			else {
+				ctx.fillStyle = averageRGB(colorlist);
+			}
+		}
+		if ((view === null || view === 4) && elements[pixel.element].state === "gas") {
+			ctx.globalAlpha = 0.5;
+			ctx.fillRect((pixel.x-1)*pixelSize, (pixel.y)*pixelSize, pixelSize*3, pixelSize);
+			ctx.fillRect((pixel.x)*pixelSize, (pixel.y-1)*pixelSize, pixelSize, pixelSize*3);
+			ctx.globalAlpha = 1;
+		}
+		else { // draw the pixel (default)
+			ctx.fillRect(pixel.x*pixelSize, pixel.y*pixelSize, pixelSize, pixelSize);
+		}
+		if (pixel.charge && view !== 2) { // Yellow glow on charge
+			if (!elements[pixel.element].colorOn) {
+				ctx.fillStyle = "rgba(255,255,0,0.5)";
+				ctx.fillRect(pixel.x*pixelSize, pixel.y*pixelSize, pixelSize, pixelSize);
+			}
+		}
+	}
+	if ((!paused) || forceTick) {pixelTicks++};
+
+	// Loop through runAfterDrawList and run each function
+	for (var i = 0; i < runAfterDrawList.length; i++) {
+		runAfterDrawList[i]();
+	};
+};
+
+
+
 var style = document.createElement('style');
 style.type = 'text/css';
 style.id = 'findStatusStylesheet';
@@ -5,17 +124,26 @@ style.innerHTML = '.findStatus { color: #E11; text-decoration: none; }';
 document.getElementsByTagName('head')[0].appendChild(style);
 
 find = false;
-
 findElement = "sand";
-
 findColorPulseTimer = 0;
+findColorPulseTimerSubTimer = 0;
 
 function marasi(number) {
 	return Math.min(255,Math.round(Math.abs(Math.sin(number) * 255)));
 };
 		
 function updateFindDescription() {
-	elements.find_toggle.desc = "<em>I'm running out of keybinds</em><br/><br/><span class=\"findStatus\">If this text is green or underlined, find mode is on.</span> <span onclick=toggleFind() style=\"color: #ff00ff;\";>Click here</span> to toggle find mode. This highlights the currently selected element.";
+	var elems = findElement;
+	if(elems instanceof Array) {
+		elems = elems.join(", ");
+	};
+	elements.find_toggle.desc = `
+<em>I'm running out of keybinds</em>
+
+
+<span class="findStatus">If this text is green or underlined, find mode is on.</span> Currently finding: ${elems} <small style="font-size:80%;">(this display does not update automatically)</small>.
+<span onclick=toggleFind() style="color: #ff00ff;";>Click here</span> to toggle find mode. This highlights the currently selected element.<br/>
+<span style='color:#FF00FF' onClick=findFilterPrompt()>Click here to configure the find filter.</span>`;
 };
 
 function toggleFind() {
@@ -29,10 +157,10 @@ function toggleFind() {
 	updateFindDescription();
 };
 
-oldDrawPixels2 = drawPixels;
-
-suffixFunction2 = function() {
-	// newCurrentPixels = shuffled currentPixels
+findHighlighting = function() {
+	if(!find) {
+		return false;
+	}
 	var newCurrentPixels = currentPixels;
 	var pixelsFirst = [];
 	var pixelsLast = [];
@@ -54,21 +182,22 @@ suffixFunction2 = function() {
 	for (var i = 0; i < pixelDrawList.length; i++) {
 		pixel = pixelDrawList[i];
 		if (pixelMap[pixel.x][pixel.y] == undefined) {continue};
-		if (find === true) { //if in find view
-			if(pixel.element === currentElement) {
-				ctx.fillStyle = "rgb(255," + marasi(findColorPulseTimer / 10) + ",0)";
-				ctx.fillRect(pixel.x*pixelSize, pixel.y*pixelSize, pixelSize, pixelSize);
-			};
+		if(findElement instanceof Array ? findElement.includes(pixel.element) : pixel.element === findElement) {
+			ctx.fillStyle = "rgb(255," + marasi(findColorPulseTimer / 10) + ",0)";
+			ctx.fillRect(pixel.x*pixelSize, pixel.y*pixelSize, pixelSize, pixelSize);
 		};
 	};
 	
-	findColorPulseTimer++;
-}
-
-drawPixels = function(forceTick=false) {
-	oldDrawPixels2(forceTick);
-	suffixFunction2();
+	findColorPulseTimerSubTimer++;
+	if(findColorPulseTimerSubTimer >= 2) {
+		findColorPulseTimer++;
+		findColorPulseTimerSubTimer = 0;
+	};
+	
+	return true;
 };
+
+runAfterDraw(findHighlighting);
 
 elements.find_toggle = {
     color: ["#000000", "#000000", "#000000", "#000000", "#ff0000", "#ff0000", "#ff0000", "#ff0000"],
@@ -76,5 +205,27 @@ elements.find_toggle = {
     behavior: behaviors.SELFDELETE,
     category: "tools",
 	excludeRandom: true,
-	desc: "<em>I'm running out of keybinds</em><br/><br/><span class=\"findStatus\">If this text is green or underlined, find mode is on.</span> <span onclick=toggleFind() style=\"color: #ff00ff;\";>Click here</span> to toggle find mode. This highlights the currently selected element.",
+	desc: `
+<em>I'm running out of keybinds</em>
+
+
+<span class="findStatus">If this text is green or underlined, find mode is on.</span> Currently finding: sand <small style="font-size:80%;">(this display does not update automatically)</small>.
+<span onclick=toggleFind() style="color: #ff00ff;";>Click here</span> to toggle find mode. This highlights the currently selected element.<br/>
+<span style='color:#FF00FF' onClick=findFilterPrompt()>Click here to configure the find filter.</span>`,
+};
+
+function findFilterPrompt() {
+	var preElement = prompt("Enter the elements you want to highlight\nSeparate multiple elements with commas");
+	if(preElement === null || preElement === "") {
+		return false;
+	};
+	if(preElement.includes(",")) {
+		preElement = preElement.split(",");
+		findElement = preElement;
+		updateFindDescription();
+		return findElement;
+	};
+	findElement = preElement;
+	updateFindDescription();
+	return findElement;
 };
