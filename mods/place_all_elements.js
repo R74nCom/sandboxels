e45395e3ecbcb2be5e1f50810349f411feab3adc
Laetitia (O-01-67) <68935009+O-01-67@users.noreply.github.com>
2022-12-21 10:53:07 -0500

    rework

--- mods/place_all_elements.js
+++ mods/place_all_elements.js
@@ -1,29 +1,30 @@
-zzzz204689123_array = Object.keys(elements);
-
-    function placeAll(limit=zzzz204689123_array.length) {
-        zzzz204689123_array = Object.keys(elements);
-        paused = true;
-        counterLol = 0;
-        loop1:
-        for(i = 1; i < height; i++) {
-            loop2:
-            for(j = 1; j < width; j++) {
-                if(isEmpty(j,i),true) {
-                  if(!outOfBounds(j,i)) {
-                    if(!zzzz204689123_array[counterLol] || counterLol > limit) {
-                        break loop1;
-                    };
-                    createPixel(zzzz204689123_array[counterLol],j,i);
-                    counterLol++;
-                  } else { break loop1; };
-                } else {
-                  if(!outOfBounds(j,i)) {
-                    deletePixel(j,i)
-                    createPixel(zzzz204689123_array[counterLol],j,i);
-                    counterLol++;
-                  } else { break loop1; };
-                }
-            };
-        };
-        paused = true;
-    };
+function placeAll(limit=null) {
+	var elementArray = Object.keys(elements);
+	if(typeof(limit) === "number") {
+		elementArray = elementArray.slice(0,limit)
+	};
+	paused = true;
+	counterLol = 0;
+	loop1:
+	for(i = 1; i < height; i++) {
+		loop2:
+		for(j = 1; j < width; j++) {
+			if(isEmpty(j,i,false)) {
+			  if(!outOfBounds(j,i)) {
+				if(!elementArray[counterLol]) {
+					break loop1;
+				};
+				createPixel(elementArray[counterLol],j,i);
+				counterLol++;
+			  } else { break loop1; };
+			} else if(!isEmpty(j,i,true)) {
+			  if(!outOfBounds(j,i)) {
+				deletePixel(j,i)
+				createPixel(elementArray[counterLol],j,i);
+				counterLol++;
+			  } else { break loop1; };
+			}
+		};
+	};
+	paused = true;
+};
