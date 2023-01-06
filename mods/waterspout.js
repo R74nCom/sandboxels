a39ee6036e26aef0f4c20e5819e29d1562028dcb
MollTheCoder <mollthecoder@programmer.net>
2023-01-06 16:04:23 -0500

    Re-implement the old water spout
    Brings back water spout from previous versions

---
+++ mods/waterspout.js
@@ -0,0 +1,26 @@
+elements.water_spout = {
+    "color": "rgb(96,99,120)",
+    "behavior": [
+        [
+            "XX",
+            "CR:water",
+            "XX"
+        ],
+        [
+            "CR:water",
+            "XX",
+            "CR:water"
+        ],
+        [
+            "XX",
+            "CR:water",
+            "XX"
+        ]
+    ],
+    "category": "special",
+    "colorObject": {
+        "r": 96,
+        "g": 99,
+        "b": 120
+    }
+}
