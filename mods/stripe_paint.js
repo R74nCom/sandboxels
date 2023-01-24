7cbb19573f9ca54d734acafb9dbdbd7a10e33aab
Laetitia (O-01-67) <68935009+O-01-67@users.noreply.github.com>
2023-01-24 13:35:02 -0500

    rework
    add arbitrary angle, phase, second color, and scale

--- mods/stripe_paint.js
+++ mods/stripe_paint.js
@@ -5,12 +5,35 @@ function averageColors(color1,color2,outputType="rgb",weight1=0.5) {
 	return convertColorFormats(theColor,outputType);
 };
 
-function stripeFunction(pixel,sineParamFunction) {
-	if(pixel.oldColor == undefined || pixel.oldColor == null) {
-		pixel.oldColor = pixel.color;
+stripeFixedDefaultProperties = {
+	color2: "rgb(0,0,0)",
+	phase: 0,
+	scale: 1,
+	angle: 0
+};
+
+stripeSpreadingProperties = {
+	color1: "It doesn't matter what I put here; I'm just sick of writing for loops.",
+	color2: "stan loona",
+};
+
+/*stripeSpreadingProperties2 = {
+	phase: 0,
+	scale: 1, :eggTF:
+	angle: 0
+};*/
+
+function stripeFunction(pixel) {
+	if(pixel.color1 == undefined || pixel.color1 == null) {
+		pixel.color1 = pixel.color;
+	};
+	for(prop in stripeFixedDefaultProperties) {
+		if(pixel[prop] == undefined || pixel[prop] == null) {
+			pixel[prop] = stripeFixedDefaultProperties[prop];
+		};
 	};
 
-	//oldColor self staining
+	//color1 and color2 self staining
 	for (var i = 0; i < adjacentCoords.length; i++) {
 		var x = pixel.x+adjacentCoords[i][0];
 		var y = pixel.y+adjacentCoords[i][1];
@@ -18,36 +41,50 @@ function stripeFunction(pixel,sineParamFunction) {
 			continue;
 		};
 		var otherPixel = pixelMap[x][y];
-		if(otherPixel.element == pixel.element && pixel.oldColor && otherPixel.oldColor) {
-			otherPixel.oldColor = averageColors(pixel.oldColor,otherPixel.oldColor);
+		for(prop in stripeSpreadingProperties) {
+			if(otherPixel.element == pixel.element && pixel[prop] && otherPixel[prop]) {
+				otherPixel[prop] = averageColors(pixel[prop],otherPixel[prop]);
+			};
 		};
+		/*for(prop in stripeSpreadingProperties2) {
+			if(otherPixel.element == pixel.element && pixel[prop] !== undefined && otherPixel[prop] !== undefined) {
+				otherPixel[prop] = pixel[prop]/2 + otherPixel[prop]/2;
+			}; :eggTF:
+		};*/
 	};
 	
-	var sineWeight = (1+Math.sin(sineParamFunction(pixel)))/2;
-	var preColor = averageColors(pixel.oldColor,"rgb(0,0,0)","json",sineWeight);
+	var radians = pixel.angle * (Math.PI / 180);
+
+	var colorNumber = (pixel.x*Math.cos(radians))+(pixel.y*Math.sin(radians));
+
+	var sineWeight = (1+Math.sin(pixel.phase + colorNumber / pixel.scale))/2;
+
+	var preColor = averageColors(pixel.color1,pixel.color2,"json",sineWeight);
 	for(colorlet in preColor) {
 		preColor[colorlet] = Math.round(preColor[colorlet]);
 	};
 	pixel.color = convertColorFormats(preColor,"rgb");
 };
 
-function horizontalSpf(pixel) {
-	return pixel.y;
-};
+stripePaintDesc = `Exactly what it says on the button.
+<br/>
+Properties: <ol>
+<li>color1: The first color of the stripe</li>
+<li>color2: The second color of the stripe (defaults to black)</li>
+<li>scale: Relative width of the stripes, compared to the default</li>
+<li>phase: Offset in the position of the stripes (π/2 = 1 stripe width)</li>
+<li>angle: Angle in degrees<ul>
+	<li>0 = vertical line</li>
+	<li>45 = bottom left to top right</li>
+	<li>90 = horizontal line</li>
+	<li>135 = top left to bottom right...</li>
+</ul></li></ol>
 
-function verticalSpf(pixel) {
-	return pixel.x;
-};
+color1 and color2 spread through striped paint like dye does with itself. <u>color1</u> can be set <em>on initial placement</em> through the color picker, but otherwise, properties must be changed through the console or with prop.js's tools.
 
-function diagonalSpf(pixel) {
-	return pixel.x+pixel.y;
-};
+<small><em style="color: #ff5555;">This does not work with HSL color and the game will black screen if one is somehow used.</em> The color conversion functions are a clusterf***.</small>`
 
-function diagonalAltSpf(pixel) {
-	return pixel.x-pixel.y;
-};
-
-elements.horizontal_stripe_paint = {
+elements.stripe_paint = {
     behavior: behaviors.LIQUID,
 	state: "liquid",
 	density: 998,
@@ -55,66 +92,18 @@ elements.horizontal_stripe_paint = {
 	stateHigh: "smoke",
     color: elements.paint.color,
 	customColor: true,
-	category: "stripe paint",
+	category: "special",
 	properties: {
-		oldColor: null
+		color1: null,
+		color2: null,
+		scale: 1,
+		phase: 0,
+		angle: 0
 	},
 	stain: elements.dye.stain,
     tick: function(pixel) {
-		stripeFunction(pixel,horizontalSpf);
+		stripeFunction(pixel);
     },
+	desc: stripePaintDesc
 };
 
-elements.vertical_stripe_paint = {
-    behavior: behaviors.LIQUID,
-	state: "liquid",
-	density: 998,
-	tempHigh: 100,
-	stateHigh: "smoke",
-    color: elements.paint.color,
-	customColor: true,
-	category: "stripe paint",
-	properties: {
-		oldColor: null
-	},
-	stain: elements.dye.stain,
-    tick: function(pixel) {
-		stripeFunction(pixel,verticalSpf);
-    },
-};
-
-elements.diagonal_stripe_paint = {
-    behavior: behaviors.LIQUID,
-	state: "liquid",
-	density: 998,
-	tempHigh: 100,
-	stateHigh: "smoke",
-    color: elements.paint.color,
-	customColor: true,
-	category: "stripe paint",
-	properties: {
-		oldColor: null
-	},
-	stain: elements.dye.stain,
-    tick: function(pixel) {
-		stripeFunction(pixel,diagonalSpf);
-    },
-};
-
-elements.diagonal_2_stripe_paint = {
-    behavior: behaviors.LIQUID,
-	state: "liquid",
-	density: 998,
-	tempHigh: 100,
-	stateHigh: "smoke",
-    color: elements.paint.color,
-	customColor: true,
-	category: "stripe paint",
-	properties: {
-		oldColor: null
-	},
-	stain: elements.dye.stain,
-    tick: function(pixel) {
-		stripeFunction(pixel,diagonalAltSpf);
-    },
-};
