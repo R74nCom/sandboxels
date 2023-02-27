2edaee4f01fac2815cf9443a62c0fda3548506cd
Laetitia (O-01-67) <68935009+O-01-67@users.noreply.github.com>
2023-02-27 15:20:40 -0500

    more detail

--- mods/changePixelDebug.js
+++ mods/changePixelDebug.js
@@ -2,7 +2,7 @@ doLog = true;
 oldChangePixel = changePixel;
 changePixel = function(pixel,element,changetemp=true) {
 	if(typeof(elements[element]) == "undefined") {
-		if(doLog) { console.error(`Something tried to change a pixel (${pixel.x},${pixel.y}) to nonexistent element "${element}"`) };
+		if(doLog) { console.error(`Something tried to change a pixel of ${pixel.element} at (${pixel.x},${pixel.y}) to nonexistent element "${element}"`) };
 		return false;
 	};
 	oldChangePixel(pixel,element,changetemp);
