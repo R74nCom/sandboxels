985bd514f7c0f511dddb706b258fbb9a4a0f9a79
mrnoodleface <86375015+mrnoodleface@users.noreply.github.com>
2023-03-21 17:27:30 -0400

    Create cool_tools.js
    my first mod

---
+++ mods/cool_tools.js
@@ -0,0 +1,29 @@
+elements.exploder = {
+    color: "#fa872f",
+    tool: function(pixel) {
+        if (pixel.element == "sand") {
+            pixel.element = "explosion"
+        }
+    },
+    category: "tools",
+
+  
+elements.radiator = {
+    color: "#248c1c",
+    tool: function(pixel) {
+        if (pixel.element == "null") {
+            pixel.element == "radiation"
+        }
+        if (pixel.element == "cloud") {
+            pixel.element = "rad_cloud"
+        }
+        if (pixel.element == "cell") {
+            pixel.element = "cancer"
+        }
+        if (pixel.element == "steam" {
+            pixel.element = "rad_steam”
+        }
+    },
+    category: "tools",
+
+//im aware that theres more radiation stuf than this
