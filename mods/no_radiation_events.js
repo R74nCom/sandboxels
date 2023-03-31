03f37ce89a67521ac333b531660e526d3a64a16e
slweeb <91897291+slweeb@users.noreply.github.com>
2023-03-31 10:40:31 -0400

    Create no_radiation_events.js

---
+++ mods/no_radiation_events.js
@@ -0,0 +1,3 @@
+window.addEventListener("load",function(){
+randomEventChoices.falling_pixel.splice(randomEventChoices.falling_pixel.indexOf("fallout"),1);randomEventChoices.explosion.splice(randomEventChoices.explosion.indexOf("radiation"),1)
+})
