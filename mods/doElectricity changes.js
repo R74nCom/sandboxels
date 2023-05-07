c32a3e58bc48bab385d021bbb3c1d726e5f89213
An Orbit <68935009+orbit-loona@users.noreply.github.com>
2023-05-07 15:54:15 -0400

    sex with heejin

--- mods/doElectricity changes.js
+++ mods/doElectricity changes.js
@@ -13,7 +13,8 @@ function doElectricity(pixel) {
 				var newPixel = pixelMap[x][y];
 				var con = elements[newPixel.element].conduct;
 				if (con == undefined) {continue}
-				if (elements[newPixel.element].noConduct?.length && elements[newPixel.element].noConduct.includes(newPixel.element)) {continue};
+				var info = elements[pixel.element];
+				if (info.noConduct && info.noConduct.includes(newPixel.element)) {continue};
 				if (Math.random() < con) { // If random number is less than conductivity
 					if (!newPixel.charge && !newPixel.chargeCD) {
 						newPixel.charge = isNaN(pixel.charge) ? 0 : pixel.charge; //Actually set it to the same charge
