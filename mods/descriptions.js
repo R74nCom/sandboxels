0fa05dd9c0749a493b812ab27d91e7db59347bfc
MollTheCoder <mollthecoder@programmer.net>
2023-07-04 01:31:47 -0400

    Update descriptions.js
    Adds a referrer to make analytics easier for me

--- mods/descriptions.js
+++ mods/descriptions.js
@@ -1,5 +1,9 @@
 console.log("descriptions.js: Loading vanilla descriptions...");
-fetch("https://sandboxels-mods.mollthecoder.repl.co/descriptions/vanilla.json")
+fetch("https://sandboxels-mods.mollthecoder.repl.co/descriptions/vanilla.json", 
+      {
+        referrer: "https://sandboxels.r74n.com/mods/descriptions.js"
+      }
+)
   .then(res=>{
     res.json().then(json=>{
     for(const element in json) {
