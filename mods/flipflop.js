89b75ae78ece04d8e4c68b1e1d127ea17e3f76f9
felixs-alt <114471609+felixs-alt@users.noreply.github.com>
2023-09-26 13:45:07 +0200

    Update flipflop.js

--- mods/flipflop.js
+++ mods/flipflop.js
@@ -17,11 +17,13 @@ elements.flipflop = {
 		if (typeof Output !== "undefined" && typeof Input !== "undefined"){
 			Output.charge = 0
 			if (pixel.powerstate == "true" && Input.charge > 0.2) {
+				Input.charge = 0
 				pixel.powerstate = "false"
       				pixel.color = "#CF300D"
 				console.log(pixel.powerstate+"y")
       				Output.charge = 0
 			} else if (pixel.powerstate == "false" && Input.charge > 0.2) {
+				Input.chare = 0
       				pixel.powerstate = "true"
 				console.log(pixel.powerstate+"n")
       				pixel.color = "#94CF0D"
