c8ea492073360d006b8933ecb6884f35392dca4a
felixs-alt <114471609+felixs-alt@users.noreply.github.com>
2023-09-27 09:00:30 +0200

    Update flipflop.js

--- mods/flipflop.js
+++ mods/flipflop.js
@@ -13,17 +13,20 @@ elements.flipflop = {
 			
     		let Output = pixelMap[pixel.x+1][pixel.y]
     		let Input = pixelMap[pixel.x-1][pixel.y]
-		if (typeof Output !== "undefined" && typeof Input !== "undefined")
-			if (pixel.powerstate == "true" && Input.charge > 0.2) {
-				Input.charge = 0
-				pixel.powerstate = "false"
-      				pixel.color = "#CF300D"
-      				Output.charge = 0
-			} else if (pixel.powerstate == "false" && Input.charge > 0.2) {
-				Input.charge = 0
-      				pixel.powerstate = "true"
-      				pixel.color = "#94CF0D"
-      				Output.charge = 5
+		if (typeof Output !== "undefined" && typeof Input !== "undefined") {
+			if (Input.charge > 0.4) {
+				if (pixel.powerstate == "true") {
+					Input.charge = 0
+					pixel.powerstate = "false"
+      					pixel.color = "#CF300D"
+      					Output.charge = 0
+				} else {
+					Input.charge = 0
+      					pixel.powerstate = "true"
+      					pixel.color = "#94CF0D"
+      					Output.charge = 5
+				}
+			}
 		}
 	}
 };
