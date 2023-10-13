5e58572fd180dc63229b83b1ad1435a44744a10b
zoeys-freind <129983142+zoeys-freind@users.noreply.github.com>
2023-10-13 13:59:50 -0500

    Create burn-up-tool.js
    a tool that burns all elements.

---
+++ mods/burn-up-tool.js
@@ -0,0 +1,9 @@
+// Create the tool (pretty basic mod)
+elements.burn_up = {
+    color: "#ff0000",
+    tool: function(pixel) {
+            pixel.element = "explosion"
+        
+    },
+    category: "tools",
+};
