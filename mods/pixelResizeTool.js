18823bf993b67ea71f7e0833586b1bc74b0541ca
slweeb <91897291+slweeb@users.noreply.github.com>
2023-11-29 20:37:29 -0500

    fix silly mod mistake

--- mods/pixelResizeTool.js
+++ mods/pixelResizeTool.js
@@ -3,7 +3,7 @@ pixelResizeButton.onclick = function(pixel) {
     let canvas_width = document.getElementById("game").width;
     let canvas_height = document.getElementById("game").height;
     let pixelSizeNeeded = prompt("How big should pixels be?");
-    if (!pixelSizeNeeded || isNAN(pixelSizeNeeded)) { alert('number is invalid!'); return; }
+    if (!pixelSizeNeeded || isNaN(pixelSizeNeeded)) { alert('number is invalid!'); return; }
     resizeCanvas(canvas_height,canvas_width, parseFloat(pixelSizeNeeded), true);
 };
 pixelResizeButton.textContent = "Resize";
