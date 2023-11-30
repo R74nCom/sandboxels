24de3ede331f19cd7a2f693e8f664cc7d05255ca
PumpkinPriest <104536541+PumpkinPriest@users.noreply.github.com>
2023-11-30 18:54:05 -0500

    random_things.js

--- mods/random_things.js
+++ mods/random_things.js
@@ -17,20 +17,18 @@ elements.legendary_energy = {
                                 "magma": { "elem1": "armageddon", "elem2": null },
                                 "void": { "elem1": "light", "elm2": null },
           },
-}
+},
 
-runAfterLoad(function() {
-        if(enabledMods.includes("liquid_energy.js") && enabledMods.includes("bananas.js")) {
-		elements.liquid_legend = {
+elements.liquid_legend = {
                         name: "liquid legend",
 		        color: "#13d644",			
                         behavior: [
                         "XX|XX|XX",
-                        "M2|EX:15>radon,radon,radon,liquid_legend%0.4 AND DL%0.2|M2",
+                        "M2|EX:15>radon,radon,legendary_energy,liquid_legend%0.4 AND DL%0.2|M2",
                         "M1|M1|M1",
 			],
                         temp: 300,
-                        category: "energy liquids",
+                        category: "liquids",
                         state: "liquid",
                         density: 2000,
                         excludeRandom: true,
@@ -38,19 +36,5 @@ runAfterLoad(function() {
                                 "magma": { "elem1": "armageddon", "elem2": null },
                          	"void": { "elem1": "light", "elm2": null },
 	                },
-                },
-		elements.banana_juice = {
-                        name: "banana juice",
-		        color: "#e0f542",
-                        behavior: behaviors.LIQUID,
-		        temp: 800,
-			category: "food",
-			state: "liquid",
-			density: 200,
-			excludeRandom: true,
-
-            }
-                 
-       }
-
+             }
 });
