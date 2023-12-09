9f7d51e6dbd83780fe43872a4c8a08d0ffb66c27
hello1089 <153386983+hello1089@users.noreply.github.com>
2023-12-09 12:22:25 -0600

    Create Doomsday_Gas.js

---
+++ mods/Doomsday_Gas.js
@@ -0,0 +1,21 @@
+"Doomsday Gas": {
+    color: "#c92a2a",
+    behavior: behaviors.GAS,
+    behaviorOn: [
+        "XX|XX|XX",
+        "XX|EX:200|XX",
+        "XX|XX|XX",
+    ],
+    conduct: 1,
+    category: "gas",
+    burn: 100,
+    burnTime: 10,
+    burnInto: "explosion",
+    tempHigh: 600,
+    stateHigh: "explosion",
+    state: "solid",
+    density: 1630,
+    excludeRandom: true,
+    alias: "poop"
+}
+
