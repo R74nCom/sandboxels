ff5633e8b757fe643564e62d87b7a4ca9eb92fc7
Pixel8882 <153401920+Pixel8882@users.noreply.github.com>
2023-12-10 01:43:34 +0100

    Create turbine.js
    first mod :) i hope it would work ?

---
+++ mods/turbine.js
@@ -0,0 +1,9 @@
+elements.turbine = {
+	color: "#610B0B",
+	behavior: behaviors.WALL,
+	category: "machines",
+	state: "solid",
+	reactions: {
+		"steam": { elem1: "null", elem2: "electric" },
+	}
+};
