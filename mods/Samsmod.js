f6b23d0692af6465d9d6ee49659038d25b36366d
samuel14320 <155481732+samuel14320@users.noreply.github.com>
2024-01-02 14:16:26 -0500

    Add files via upload

---
+++ mods/Samsmod.js
@@ -0,0 +1,9 @@
+elements.HighMeltPoint = {
+	color: ["#d35e77", "#cd7394". "#b25325" "#cd7394"],
+	behavior: behaviors.WALL,
+	categpry: "solids",
+	temphigh: 10000,
+	statelow: "molten_iron",
+	density:1.7
+	temp: 0
+}
