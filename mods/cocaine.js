5e78421aaf34cffc1e6487497e4c0482c7f0050e
Ahahahahahahahahaa <94193926+Ahahahahahahahahaa@users.noreply.github.com>
2024-01-03 16:06:23 +0200

    Create cocaine.js

---
+++ mods/cocaine.js
@@ -0,0 +1,10 @@
+elements.cocaine = {
+    color: ["#FFFFFF"],
+    behavior: behaviors.POWDER,
+    category: "powders",
+    state: "solid",
+    density: 265,
+    burn: 300,
+    burnTime: 300,
+    burnInto: ["carbon_dioxide"],
+  };
