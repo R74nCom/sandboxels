85ba90bef59b7f2c30204a75828fae76a7bebf6b
HACKERPRO908 <130792075+HACKERPRO908@users.noreply.github.com>
2024-01-20 08:13:24 +0000

    Update 1.1 more_gold.js
    adds gold tree
    
    Signed-off-by: HACKERPRO908 <130792075+HACKERPRO908@users.noreply.github.com>

--- mods/more_gold.js
+++ mods/more_gold.js
@@ -16,3 +16,25 @@ elements.molten_green_gold = {
     conduct: 0.87,
 	Hidden: true
 };
+elements.gold_tree = {
+    color: "#f2e013",
+    tick: function (pixel) {
+        if (pixel.h > 0 && pixelTicks % (50+pixel.h) === 0) {
+
+            if (isEmpty(pixel.x-1,pixel.y)) {
+                createPixel("gold",pixel.x-1,pixel.y);
+                pixelMap[pixel.x-1][pixel.y].h = pixel.h-2;
+            }
+            if (isEmpty(pixel.x+1,pixel.y)) {
+                createPixel("gold",pixel.x+1,pixel.y);
+                pixelMap[pixel.x+1][pixel.y].h = pixel.h-2;
+            }
+        }
+        doDefaults(pixel);
+    },
+    category:"solid",
+    breakInto: "gold",
+    state: "solid",
+    density: 8000,
+    seed: "gold_coin",
+},
