9185dbdde570ab051344d2cf5e6758dd7904b04d
ACaracalCat <115324965+ACaracalCat@users.noreply.github.com>
2024-01-22 16:03:32 -0600

    Create crazy.js

---
+++ mods/crazy.js
@@ -0,0 +1,18 @@
+// red things
+elements.redliquidthing = {
+  name: "Red Liquid Thing",
+  color: ["#ff0000","#ff5100"],
+  behavior: behaviors.LIQUID,
+  category: "crazy",
+  state: "liquid",
+  density: 265,
+  tempLow: 1,
+  stateLow: "redfoodthing",
+};
+elements.redfoodthing = {
+  name: "Red Food Thing",
+  color: ["#910000"],
+  behavior: behaviors.POWDER,
+  category: "crazy",
+  state: "solid",
+};
