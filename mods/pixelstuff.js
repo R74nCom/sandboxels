0faca898a71b2dfb8ad528d9f0f6e9e49e259409
HACKERPRO908 <130792075+HACKERPRO908@users.noreply.github.com>
2024-01-27 20:31:06 +0000

    pixelstuff.js
    Signed-off-by: HACKERPRO908 <130792075+HACKERPRO908@users.noreply.github.com>

---
+++ mods/pixelstuff.js
@@ -0,0 +1,32 @@
+elements.east_korea = {
+    color: "#4287f5",
+    behavior: behaviors.SOLID,
+    category: "lol",
+    viscosity: 35,
+    state: "solid",
+    density: 9999,
+};
+elements.blockz = {
+    color: "#42f5f2",
+    behavior: behaviors.SOLID,
+    category: "lol",
+    viscosity: 35,
+    state: "solid",
+    density: 9999,
+};
+elements.smoor = {
+    color: "#48f542",
+    behavior: behaviors.POWDER,
+    category: "lol",
+    viscosity: 35,
+    state: "solid",
+    density: 9999,
+};
+elements.cake_apple = {
+    color: ["#00e649","#e62e00"],
+    behavior: behaviors.POWDER,
+    category: "lol",
+    viscosity: 35,
+    state: "solid",
+    density: 9999,
+};
