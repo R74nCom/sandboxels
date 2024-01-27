2a36c0968f8bd814dc574339bec246e49e2a2f0e
idk73248 <86132722+idk73248@users.noreply.github.com>
2024-01-27 15:10:36 +0000

    Add files via upload

---
+++ all_around_fillers.js
@@ -0,0 +1,71 @@
+elements.top_filler = {
+    color: "#ae4cd9",
+    behavior: [
+        ["XX","CL","XX"],
+        ["XX","XX","XX"],
+        ["XX","XX","XX"]
+    ],
+    category:"special",
+    excludeRandom: true,
+    reactions: {
+        "neutron": { elem1:"lattice" },
+        "proton": { elem1:"vertical" },
+        "electric": { elem1:"horizontal" },
+        "positron": { elem1:"vertical" },
+        "plasma": { elem1:"armageddon", tempMin:500, charged:true },
+    }
+},
+
+elements.bottom_filler = {
+    color: "#ae4cd9",
+    behavior: [
+        ["XX","XX","XX"],
+        ["XX","XX","XX"],
+        ["XX","CL","XX"]
+    ],
+    category:"special",
+    excludeRandom: true,
+    reactions: {
+        "neutron": { elem1:"lattice" },
+        "proton": { elem1:"vertical" },
+        "electric": { elem1:"horizontal" },
+        "positron": { elem1:"vertical" },
+        "plasma": { elem1:"armageddon", tempMin:500, charged:true },
+    }
+},
+
+elements.right_filler = {
+    color: "#ae4cd9",
+    behavior: [
+        ["XX","XX","XX"],
+        ["XX","XX","CL"],
+        ["XX","XX","XX"]
+    ],
+    category:"special",
+    excludeRandom: true,
+    reactions: {
+        "neutron": { elem1:"lattice" },
+        "proton": { elem1:"vertical" },
+        "electric": { elem1:"horizontal" },
+        "positron": { elem1:"vertical" },
+        "plasma": { elem1:"armageddon", tempMin:500, charged:true },
+    }
+},
+
+elements.left_filler = {
+    color: "#ae4cd9",
+    behavior: [
+        ["XX","XX","XX"],
+        ["CL","XX","XX"],
+        ["XX","XX","XX"]
+    ],
+    category:"special",
+    excludeRandom: true,
+    reactions: {
+        "neutron": { elem1:"lattice" },
+        "proton": { elem1:"vertical" },
+        "electric": { elem1:"horizontal" },
+        "positron": { elem1:"vertical" },
+        "plasma": { elem1:"armageddon", tempMin:500, charged:true },
+    }
+}
