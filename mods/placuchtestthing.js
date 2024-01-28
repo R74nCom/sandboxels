4baa9d5ff7942894ca2318792af3267cf2828dc4
Placuch12 <77850293+Placuch12@users.noreply.github.com>
2024-01-28 11:21:14 +0100

    Added many stuff.
    Elements:
    -TestSolid
    -TestIce
    -TestGas
    -TestMud
    -TestMolten
    
    Reactions:
    -TestDust + TestLiquid = TestMud
    -TestSolid + Smash = TestDust
    -TestSolid or TestDust + Heat = TestMolten
    -TestMolten + Cool = TestSolid
    -TestMud + Heat = TestDust + TestGas
    -TestLiquid + Heat = TestGas
    -TestLiquid + Cool = TestIce
    -TestIce + Smash = TestDust
    -probably something more but i forgot

--- mods/placuchtestthing.js
+++ mods/placuchtestthing.js
@@ -1,17 +1,92 @@
 elements.testdust = {
     color: "#CC8811",
     behavior: behaviors.POWDER,
+    temp: 20,
+    tempHigh: 1200,
+    stateHigh: "testmolten",
     category: "powders",
     viscosity: 10000,
     state: "powder",
+    reactions: {
+        "testliquid" : { elem1: "testmud", elem2: null},
+    },
     density: 1250,
+};
+
+elements.testmud = {
+    color: "#BB5501",
+    behavior: behaviors.STURDYPOWDER,
+    temp: 20,
+    tempHigh: 150,
+    stateHigh: ["testdust","testdust","testgas","testdust","testgas"],
+    category: "land",
+    viscosity: 10000,
+    state: "powder",
+    density: 1450,
 };
 
 elements.testliquid = {
     color: "#EEAA33",
     behavior: behaviors.LIQUID,
+    temp: 20,
+    tempHigh: 150,
+    stateHigh: "testgas",
+    tempLow: -5,
+    stateLow: "testice",
     category: "liquids",
-    viscosity: 100000,
+    viscosity: 10000,
     state: "liquid",
-    density: 1150,
+    density: 1050,
+};
+
+elements.testmolten = {
+    color: ["#FE6D01","#FF8C00","#FF4D00"],
+    behavior: behaviors.MOLTEN,
+    temp: 1500,
+    tempLow: 1200,
+    stateLow: "testsolid",
+    category: "liquids",
+    viscosity: 10000,
+    state: "liquid",
+    density: 1250,
+};
+
+elements.testsolid = {
+    color: "#DC9831",
+    behavior: behaviors.WALL,
+    temp: 20,
+    tempHigh: 1200,
+    stateHigh: "testmolten",
+    category: "solids",
+    viscosity: 10000,
+    state: "solid",
+    density: 1250,
+    breakInto: "testdust",
+    noMix: true,
+};
+
+elements.testgas = {
+    color: "#FFBB55",
+    behavior: behaviors.GAS,
+    temp: 200,
+    tempLow: 145,
+    stateLow: "testliquid",
+    category: "gases",
+    viscosity: 10000,
+    state: "gas",
+    density: 1050,
+};
+
+elements.testice = {
+    color: "#FEBA53",
+    behavior: behaviors.WALL,
+    temp: -50,
+    tempHigh: -5,
+    stateHigh: "testliquid",
+    category: "solids",
+    viscosity: 10000,
+    state: "solid",
+    density: 1250,
+    breakInto: "testdust", // thought it would be cool if all the elements would be connected in some way
+    noMix: true,
 };
