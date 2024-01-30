5f43014dbd8ae7a69deb86ddf3892e1f9da3e02e
BloxParkCreations <152893480+BloxParkCreations@users.noreply.github.com>
2024-01-30 13:00:28 -0500

    Create chickenbroth.js

---
+++ mods/chickenbroth.js
@@ -0,0 +1,10 @@
+elements.chickenBroth = {
+  color: #BEB071,
+  behavior: behaviors.LIQUID,
+  category: "liquids",
+  viscosity: 17.71,
+  state: "liquid",
+  density: 1.07,
+  tempLow: 0,
+  tempHigh: 37;
+};
