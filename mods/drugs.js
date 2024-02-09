645bd04fe28c8a679a077ecaf15259ca4bbbe749
0rangy <86059513+0rangy@users.noreply.github.com>
2024-02-09 10:02:38 -0500

    Update drugs.js

--- mods/drugs.js
+++ mods/drugs.js
@@ -6,27 +6,29 @@ Making drugs isn't information that is easy to find on the web, therefor this mo
 */
 
 
-elements.methamphetamine = {
+elements.drugs_methamphetamine = {
   color: ["#F0F8FE","#FFFFFF","#CDEDFA"],
   name: "Methamphetamine",
   behavior: behaviors.POWDER,
   category: "drugs",
   state: "solid",
 };
-elements.ephedrine = {
+elements.drugs_ephedrine = {
   name: "Ephedrine",
   color: ["#A091C9","#B29AD1","#BC9ED6"],
   behavior: behaviors.POWDER,
   category: "drugs",
   state: "solid",
+  tempHigh: 180,
+  stateHigh: "ephedrine_gas",
   reactions: {
-    "hydroiodic_acid": { "elem1": "methamphetamine", "elem2": null }
+    "drugs_hydroiodic_acid": { "elem1": "drugs_methamphetamine", "elem2": null }
   }
 };
-
-elements.iodine_gas = {
-  name: "Iodine Gas",
-  color: ["#282C35"],
+elements.drugs_ephedrine_gas = {
+  name: "Ephedrine Gas",
+  color: ["#A091C9","#B29AD1","#BC9ED6"],
+  hidden: true,
   behavior: [
         ["M2","M1","M2"],
         ["M1","DL%5","M1"],
@@ -35,7 +37,20 @@ elements.iodine_gas = {
   category: "drugs",
   state: "gas"
 };
-elements.iodine = {
+
+elements.drugs_iodine_gas = {
+  name: "Iodine Gas",
+  color: ["#282C35"],
+  hidden: true,
+  behavior: [
+        ["M2","M1","M2"],
+        ["M1","DL%5","M1"],
+        ["M2","M1","M2"]
+    ],
+  category: "drugs",
+  state: "gas"
+};
+elements.drugs_iodine = {
   name: "Iodine",
   color: ["#000000","#282C35","#100C08"],
   behavior: behaviors.POWDER,
@@ -45,10 +60,10 @@ elements.iodine = {
   tempHigh: 183,
   stateHigh: "iodine_gas",
   reactions: {
-    "hypophosphorous_acid": { "elem1": "hydroiodic_acid", "elem2": null }
+    "drugs_hypophosphorous_acid": { "elem1": "drugs_hydroiodic_acid", "elem2": null }
   }
 };
-elements.hydroiodic_acid = {
+elements.drugs_hydroiodic_acid = {
   name: "Hydroiodic Acid",
   color: ["#FFFEE0", "#FFFEC8", "#FFFDAF"],
   behavior: behaviors.LIQUID,
@@ -56,17 +71,17 @@ elements.hydroiodic_acid = {
   density: 1500,
   state: "liquid"
 };
-elements.phosphorus = {
+elements.drugs_phosphorus = {
   name: "Phosphorus",
   color: ["#F8F8F8"],
-  behaviour: behaviors.POWDER,
+  behavior: behaviors.POWDER,
   category: "drugs",
   state: "solid",
   reactions: {
-    "water": { "elem1": "hypophosphorous_acid", "elem2": null }
+    "water": { "elem1": "drugs_hypophosphorous_acid", "elem2": null }
   }
 };
-elements.hypophosphorous_acid = {
+elements.drugs_hypophosphorous_acid = {
   name: "Hypophosphorous Acid",
   color: ["#3B3C36"],
   behavior: behaviors.LIQUID,
