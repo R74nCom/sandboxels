7ea492bb011ced0778397483db83732ebd953c1e
NeverGonaLetYouCry <137004284+NeverGonaLetYouCry@users.noreply.github.com>
2024-02-10 17:33:32 +0100

    Create lighter_block.js

---
+++ mods/lighter_block.js
@@ -0,0 +1,10 @@
+elements.lightercube = {
+  color: ["#ffffff"],
+  behavior: behaviors.WALL,
+  category: "solids",
+  state: "solid",
+  density: 265,
+  burn: 1000,
+  burnTime: 300,
+  burnInto: ["smoke"],
+};
