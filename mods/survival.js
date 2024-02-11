19eff2e9babd56b0cf18f6477365ba32dfd29056
slweeb <91897291+slweeb@users.noreply.github.com>
2024-02-11 14:08:15 -0500

    Update survival.js

--- mods/survival.js
+++ mods/survival.js
@@ -71,6 +71,7 @@ runAfterAutogen(function(){
     delete elements.paint.category;
     delete elements.lookup.category;
     delete elements.pick;
+    delete elements.prop;
     elements.radiation.category = "tools";
     for (var element in elements) {
         if (elements[element].category !== "tools") {
@@ -114,7 +115,7 @@ elements.cloner.tick = function(pixel) {
         }
     }
 };
-elements.cloner.ignore = elements.cloner.ignore.concat(["gold","gold_coin","molten_gold"]);
+elements.cloner.ignore = elements.cloner.ignore.concat(["gold","gold_coin","molten_gold","sun","supernova"]);
 elements.cloner.desc = "You can only clone one element at a time!"
 
 elements.smash.tool = function(pixel) {
@@ -123,7 +124,7 @@ elements.smash.tool = function(pixel) {
         // times 0.25 if not shiftDown else 1
         if (Math.random() < (elements[pixel.element].hardness || 1) * (shiftDown ? 1 : 0.25)) {
             var breakInto = elements[pixel.element].breakInto;
-            if (elements[pixel.element].seed) {
+            if (elements[pixel.element].seed && (!breakInto || Math.random() < 0.5)) {
                 if (Math.random() < 0.2) {
                     breakInto = elements[pixel.element].seed;
                 }
@@ -181,6 +182,7 @@ elementWorth = {
     "fire": 0,
     "smoke": 0,
     "plasma": 0,
+    "light": 0,
     "petal": -1,
     "cell": -1,
     "cancer": -1,
