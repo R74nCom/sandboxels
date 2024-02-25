df94f0bf43dd9d21209eab2c180537691c602a5b
DaviStudios <112717418+DaviStudios@users.noreply.github.com>
2024-02-25 22:21:58 +0200

    Create kopalstuff.js

---
+++ mods/kopalstuff.js
@@ -0,0 +1,10 @@
+elements.lemon = {
+	color: "#fcc603",
+	behavior: behaviors.POWDER,
+	category: "first",
+	state: "solid",
+  breakInto: "Juice",
+  reactions = {
+    "sugar": { elem1: null, elem2: "candy" }
+  }
+};
