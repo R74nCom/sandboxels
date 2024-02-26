12dbde77a205025f8c5b69432b8705788068d806
DaviStudios <112717418+DaviStudios@users.noreply.github.com>
2024-02-26 15:01:29 +0200

    Update kopalstuff.js

--- mods/kopalstuff.js
+++ mods/kopalstuff.js
@@ -1,17 +1,37 @@
-elements.candy_powder = {
-  color: "#cffaf1",
-  behavior: behaviors.POWDER,
-  category: null,
+elements.sweetwater = {
+  color: "#8ae7eb",
+  behavior: behaviors.liquid,
+  category: "davko",
+  state: "solid",
+  reactions: {
+    "soda": { elem1: null, elem2: "syrup" }
+  },
+};
+
+elements.syrup = {
+  color: "#d9d2c3",
+  behavior: behaviors.liquid,
+  category: "davko",
   state: "solid",
 };
 
-elements.lemon = {
-  color: "#fcc603",
-  behavior: behaviors.POWDER,
-  category: "food",
+elements.orangesoda = {
+  color: "#b8820d",
+  behavior: behaviors.liquid,
+  category: "davko",
+  state: "solid",
+  reactions: {
+    "sugar": { elem1: null, elem2: "syrup" }
+  },
+};
+
+elements.orange = {
+  color: "#eda813",
+  behavior: behaviors.powder,
+  category: "davko",
   state: "solid",
   breakInto: "juice",
   reactions: {
-    "sugar": { elem1: null, elem2: "candy_powder" }
+    "syrup": { elem1: null, elem2: "orangesoda" }
   },
 };
