555f7be9b17199c03249660a15f115d0570a2d5b
HACKERPRO908 <130792075+HACKERPRO908@users.noreply.github.com>
2024-02-29 18:16:32 +0000

    Update more_gold.js
    Signed-off-by: HACKERPRO908 <130792075+HACKERPRO908@users.noreply.github.com>

--- mods/more_gold.js
+++ mods/more_gold.js
@@ -10,6 +10,9 @@
 // adds gold_rust 
 // added reactions for the golds
 
+//1.1.1 update
+// adds uranium_gold
+
 elements.green_gold = {
     color: ["#94c7a3","#7bb298","#94c7a3"],
 	reactions: {
@@ -77,7 +80,31 @@ elements.gold_rust = {
     hardness: 0.3
 };
 
-elements.gold.reactions["fire"] = { elem1: null, elem2: "gold_rust" };
-elements.rose_gold.reactions["fire"] = { elem1: null, elem2: "gold_rust" };
-elements.purple_gold.reactions["fire"] = { elem1: null, elem2: "gold_rust" };
-elements.blue_gold.reactions["fire"] = { elem1: null, elem2: "gold_rust" };
+elements.uranium_gold = {
+    color: ["#bcf542","#7fc92a","#c9bf2a"],
+    behavior: [
+       "XX|CR:radiation|XX",
+       "XX|XX|XX",
+       "XX|XX|XX",
+    ],
+    tempHigh: 1064,
+	reactions: {
+        "water": { elem1:"rad_steam", chance:0.1 },
+        "salt_water": { elem1:"gold_rust", chance:0.005 },
+        "dirty_water": { elem1:"gold_rust", chance:0.04 },
+        "pool_water": { elem1:"gold_rust", chance:0.04 },
+        "sugar_water": { elem1:"gold_rust", chance:0.0035 },
+        "seltzer": { elem1:"gold_rust", chance:0.006 },
+        "alcohol": { elem1:"gold_rust", chance:0.03 },
+        "blood": { elem1:"gold_rust", chance:0.003 },
+        "infection": { elem1:"gold_rust", chance:0.003 },
+        "antibody": { elem1:"gold_rust", chance:0.003 },
+        "fire": { elem1:"gold_rust", chance:0.0025 },
+		"oxygen": { elem1:"gold_rust", chance:0.05 },
+    },
+    category: "solids",
+    density: 19300,
+    conduct: 0.81,
+    hardness: 0.25,
+    breakInto: "uranium"
+};
