ccba7978810adf09de209c9e503cf713e510d5a7
HACKERPRO908 <130792075+HACKERPRO908@users.noreply.github.com>
2024-03-19 15:55:21 +0000

    Create tech.js
    Signed-off-by: HACKERPRO908 <130792075+HACKERPRO908@users.noreply.github.com>

---
+++ mods/tech.js
@@ -0,0 +1,55 @@
+elements.monitor_case = {
+    color: "#4a4848",
+    behavior: behaviors.SOLID,
+    category: "Tech",
+    state: "solid",
+    density: 500,
+};
+
+elements.pc_core = {
+    color: "#f0cd43",
+    behavior: [
+        "XX|SH|XX",
+        "SH|XX|SH",
+        "XX|SH|XX",
+    ],
+    category: "Tech",
+    tempHigh: 8000,
+    stateHigh: ["molten_steel","explosion","molten_iron"]
+};
+
+elements.red_wire = {
+    color: "#ff3d1f",
+    behavior: behaviors.WALL,
+    category: "Tech",
+    insulate: true,
+    conduct: 3,
+    noMix: true
+};
+
+elements.green_wire = {
+    color: "#66c22d",
+    behavior: behaviors.WALL,
+    category: "Tech",
+    insulate: true,
+    conduct: 3,
+    noMix: true
+};
+
+elements.blue_wire = {
+    color: "#1f81cc",
+    behavior: behaviors.WALL,
+    category: "Tech",
+    insulate: true,
+    conduct: 3,
+    noMix: true
+};
+
+elements.electrogalvanized = {
+    color: "#6c6e70",
+    behavior: behaviors.SOLID,
+    category: "Tech",
+    state: "solid",
+    conduct: 2,
+    density: 7850,
+};
