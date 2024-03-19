2b64e845893e99ed3d150e2be4fedc6fcce0ea04
GuyWhoGotSpringlocked <163777207+GWhoGS@users.noreply.github.com>
2024-03-19 09:02:37 -0400

    Purple Guy's Mod
    v0.1
    -Adds 15 new elements!
    -Keep in mind that I'm limited to some stuff, and I also got a lot of my code from Ryan.

--- purple_guys_mod.js
+++ purple_guys_mod.js
@@ -26,6 +26,35 @@ elements.mega_bomb = {
     cooldown: defaultCooldown
 }
 
+elements.ultra_bomb = { 
+    color: "#79910a", 
+    behavior: [
+        "XX|EX:500>plasma2|XX", 
+        "XX|XX|XX",
+        "M2|M1 AND EX:500>plasma2|M2",
+    ],
+    category: "Purple Guy's Mod", 
+    state: "solid",
+    density: 1300,
+    excludeRandom: true,
+    cooldown: defaultCooldown
+}
+
+elements.ice_cream_bomb = { 
+    color: "#ffffff", 
+    behavior: [
+        "XX|EX:20>ice_cream|XX", 
+        "XX|XX|XX",
+        "M2|M1 AND EX:20>ice_cream|M2",
+    ],
+    category: "Purple Guy's Mod", 
+    state: "solid",
+    density: 1300,
+    excludeRandom: true,
+    cooldown: defaultCooldown
+}
+
+
 elements.dirty_sand = { 
     color: ["#a35222", "#a15122", "#7e3f1a", "#f4ba9a", "#fed3ba", "#f2b796"],
     behavior: behaviors.POWDER,
@@ -63,6 +92,74 @@ elements.reverse_soda = {
     density: 1030
 }
 
+elements.negatively_dense_liquid = { 
+    color: "#94ffd1",
+    behavior: behaviors.LIQUID	,
+    category: "Purple Guy's Mod", 
+    state: "liquid",
+    density: -2000
+}
+
+elements.plasma2 = { 
+    color: "#caf7ff",
+    behavior: behaviors.DGAS	,
+    category: "Purple Guy's Mod", 
+    state: "gas",
+    density: 50000,
+    temp:500000
+}
+
+elements.negative_fire = { 
+    color: ["#a6bfc4", "#6bd2e4", "#b8dee4", "#32cae4"],
+    behavior: behaviors.DGAS	,
+    category: "Purple Guy's Mod", 
+    state: "gas",
+    density: -0.1,
+    temp:-20000
+}
+
+elements.coldest_bomb = { 
+    color: "#fffff0", 
+    behavior: [
+        "XX|EX:55>negative_fire|XX", 
+        "XX|XX|XX",
+        "M2|M1 AND EX:55>negative_fire|M2",
+    ],
+    category: "Purple Guy's Mod", 
+    state: "solid",
+    density: 1300,
+    excludeRandom: true,
+    cooldown: defaultCooldown
+}
+
+elements.reinforced_wall = { 
+    color: "#ffff01", 
+    behavior: behaviors.WALL,
+    category: "Purple Guy's Mod", 
+    state: "solid",
+    density: 1300,
+    hardness:10^81
+}
+
+elements.unreinforced_wall = { 
+    color: "#ffff82", 
+    behavior: behaviors.WALL,
+    category: "Purple Guy's Mod", 
+    state: "solid",
+    density: 1300,
+    hardness:0
+}
+
+elements.reinforced_liquid_wall = { 
+    color: "#ffff01", 
+    behavior: behaviors.WALL,
+    category: "Purple Guy's Mod", 
+    state: "liquid",
+    density: 1300,
+    hardness:10^81
+}
+
+
 if (!elements.sand.reactions) { elements.sand.reactions = {} }
 elements.sand.reactions.dirt = { elem1:"dirty_sand", elem2:"dirty_sand" }
 
@@ -71,3 +168,6 @@ elements.dirty_sand.reactions.water = { elem1:"wet_dirty_sand", elem2:"wet_dirty
 
 if (!elements.soda.reactions) { elements.soda.reactions = {} }
 elements.soda.reactions.reverse_gravity_liquid = { elem1:"reverse_soda", elem2:"reverse_soda" }
+
+if (!elements.wall.reactions) { elements.wall.reactions = {} }
+elements.wall.reactions.plasma2 = { elem1:"plasma2", elem2:"plasma2" }
