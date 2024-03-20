b39ddf8202b9448847165b958e4abd41e3fde31f
Squiysh <150573938+Squiysh@users.noreply.github.com>
2024-03-20 16:15:51 +0700

    Test.js
    Test

---
+++ mods/Test.js
@@ -0,0 +1,41 @@
+/*elements.warm = {
+    color: "#7fff7f",
+    tool: function(pixel) {
+        pixel.temp = 20;
+		pixelTempCheck(pixel)
+    },
+    category: "tools",
+};*/
+//warm is redundant due to room_temp
+elements.ultraheat = {
+    color: ["#ff0000", "#ffbf4f", "#ff0000", "#ffbf4f", "#ff0000", "#ffbf4f"],
+    tool: function(pixel) {
+        if(shiftDown) { pixel.temp += (9000 * (1 + shiftDown)) } else { pixel.temp += 9000 }
+		pixelTempCheck(pixel)
+    },
+    category: "tools",
+};
+elements.ultracool = {
+    color: ["#0000ff", "#4fbfff", "#0000ff", "#4fbfff", "#0000ff", "#4fbfff"],
+    tool: function(pixel) {
+        if(shiftDown) { pixel.temp -= (9000 * (1 + shiftDown)) } else { pixel.temp -= 9000 }
+		pixelTempCheck(pixel)
+    },
+    category: "tools",
+};
+elements.na_ntemp = {
+    color: ["#000000", "#ff00ff", "#000000", "#ff00ff"],
+    tool: function(pixel) {
+        pixel.temp = NaN;
+		pixelTempCheck(pixel)
+    },
+    category: "tools",
+};
+elements.inftemp = {
+    color: ["#ff0000", "#ffffff", "#ff0000", "#ffffff", "#ff0000", "#ffffff", "#ff0000", "#ffffff", "#ff0000", "#ffffff", "#ff0000", "#ffffff", "#ff0000", "#ffffff", "#ff0000", "#ffffff", "#ff0000", "#ffffff", "#ff0000", "#ffffff", "#ff0000", "#ffffff", "#ff0000", "#ffffff", "#ff0000", "#ffffff", "#ff0000", "#ffffff", "#ff0000", "#ffffff", "#ff0000", "#ffffff", "#ff0000", "#ffffff", "#ff0000", "#ffffff", "#ff0000", "#ffffff", "#ff0000", "#ffffff"],
+    tool: function(pixel) {
+        pixel.temp = Infinity;
+		pixelTempCheck(pixel)
+    },
+    category: "tools",
+};
