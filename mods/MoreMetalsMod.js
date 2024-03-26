c572c5a6d09663ea5c9c969eee607e3763842947
Ethanmejia <123780881+Ethanmejia@users.noreply.github.com>
2024-03-26 18:48:00 -0500

    Update MoreMetalsMod.js

--- mods/MoreMetalsMod.js
+++ mods/MoreMetalsMod.js
@@ -4,17 +4,18 @@ elements.Reinforced_Steel = {
     category: "solids",
     state:  "solid",
     density: 720,
-    tempHighHigh: 10000,
+    tempHigh: 10000,
     stateHigh: "Molten_Reinforced_Steel",
 };
 elements.Molten_Reinforced_Steel = {
     color: "#e64e17",
-    behavior: behaviors.LIQUID,
+    behavior: behaviors.SUPERFLUID,
     category: "states",
     state:  "liquid",
     density: 7200,
     stateLow: "Reinforced_Steel",
-    tempLow: 9980
+    tempLow: 9980,
+    hidden:true,
 };
 elements.Atomic_Acid = {
     color: "#22ff00",
@@ -24,6 +25,18 @@ elements.Atomic_Acid = {
     density: 7200,
     reactions: {
         "sand": {elem1: "Atomic_Acid", elem2: null},
+        "dirt": {elem1: "Atomic_Acid", elem2: null},
+        "wet_sand": {elem1: "Atomic_Acid", elem2: null},
+        "mud": {elem1: "Atomic_Acid", elem2: null},
+        "rock": {elem1: "Atomic_Acid", elem2: null},
+        "rock_wall": {elem1: "Atomic_Acid", elem2: null},
+        "packed_sand": {elem1: "Atomic_Acid", elem2: null},
+        "mudstone": {elem1: "Atomic_Acid", elem2: null},
+        "snow": {elem1: "Atomic_Acid", elem2: null},
+        "gravel": {elem1: "Atomic_Acid", elem2: null},
+        "clay": {elem1: "Atomic_Acid", elem2: null},
+        "clay_soil": {elem1: "Atomic_Acid", elem2: null},
+        "premafrost": {elem1: "Atomic_Acid", elem2: null},
         "dirt": {elem1: "Atomic_Acid", elem2: null},
         "wet_sand": {elem1: "Atomic_Acid", elem2: null},
         "mud": {elem1: "Atomic_Acid", elem2: null},
