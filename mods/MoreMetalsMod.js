757d69c4df614dea9221998319ce8acad2e284b1
Ethan <123780881+Ethanmejia@users.noreply.github.com>
2024-03-26 14:23:12 -0400

    Update MoreMetalsMod.js

--- mods/MoreMetalsMod.js
+++ mods/MoreMetalsMod.js
@@ -1,7 +1,17 @@
-elements.HelloWorld = {
-    color: "#100D5E",
+elements.Reinforced_Steel = {
+    color: "#807575",
     behavior: behaviors.WALL,
-    category: "land",
+    category: "solid",
     state:  "solid",
     density: 720,
+    stateHigh: 10000,
+    burnInto: "Molten_Reinforced_Steel"
+};
+elements.Molten_Reinforced_Steel = {
+    color: "#807575",
+    behavior: behaviors.LIQUID,
+    category: "solid",
+    state:  "solid",
+    density: 720,
+    stateLow: 9990,
 };
