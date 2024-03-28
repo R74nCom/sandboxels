4d0d0b5257bcb3afe397579b953add6ca3a6ee84
Ethanmejia <123780881+Ethanmejia@users.noreply.github.com>
2024-03-28 18:25:16 -0500

    Update Fantasy_metals.js

--- mods/Fantasy_metals.js
+++ mods/Fantasy_metals.js
@@ -19,3 +19,34 @@ elements.Adamantium = {
     state:  "solid",
     density: 200,
 };
+elements.Orichalcum = {
+    color: ["#de55ed", "#bc13cf", "#8c3396", "#a952b3", "#bc08d1"],
+    behavior: behaviors.POWDER,
+    category: "powders",
+    state:  "solid",
+    density: 200,
+};
+elements.Brightsteel = {
+    color: ["#c4d10f", "#c2cc35", "#dde386", "#778003", "#dff005"],
+    behavior: 
+    [
+        ["CR:light%0.05","CR:light%0.05","CR:light%0.05"],
+        ["CR:light%0.05","XX","CR:light%0.05"],
+        ["M2","M1","M2"]
+    ],
+    category: "powders",
+    state:  "solid",
+    density: 200,
+};
+elements.Darksteel = {
+    color: ["#8f8786", "#2e2625", "#120b0b", "#000000", "#000000"],
+    behavior: 
+    [
+        ["CR:light%0.05","CR:light%0.05","CR:light%0.05"],
+        ["CR:light%0.05","XX","CR:light%0.05"],
+        ["M2","M1","M2"]
+    ],
+    category: "powders",
+    state:  "solid",
+    density: 200,
+};
