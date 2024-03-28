2da9cd922e882fc054b1ca4e1294511e345db807
Ethanmejia <123780881+Ethanmejia@users.noreply.github.com>
2024-03-27 21:19:55 -0500

    Get Behavior working!

--- mods/AtomicAcid.js
+++ mods/AtomicAcid.js
@@ -19,6 +19,12 @@ elements.Molten_Reinforced_Steel = {
 };
 elements.Atomic_Acid = {
     color: "#22ff00",
+    ignore: ["Atomic_Acid"],
+    behavior: [
+		"CH:Atomic_Acid|DL AND CH:Atomic_Acid|CH:Atomic_Acid",
+		"DL AND M2 AND CH:Atomic_Acid|XX|DL AND M2 AND CH:Atomic_Acid",
+		"M1 AND CH:Atomic_Acid|DL AND M1 AND CH:Atomic_Acid|M1 AND CH:Atomic_Acid",
+	],
     category: "liquids",
     state:  "liquid",
     density: 1,
