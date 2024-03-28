70064165b8a8fa8cf2790bd4efc898d5928680fa
Ethanmejia <123780881+Ethanmejia@users.noreply.github.com>
2024-03-28 17:53:24 -0500

    Create Fantasy_metals.js

---
+++ mods/Fantasy_metals.js
@@ -0,0 +1,21 @@
+elements.Mithril = {
+    color: ["#66dede", "#78c4c4", "#6bbfa7", "#51fca9", "#6598b5", "#86a1db", "#61ad6e"],
+    behavior: behaviors.POWDER,
+    category: "powders",
+    state:  "solid",
+    density: 200,
+};
+elements.Palladium = {
+    color: ["#e67c47", "#61280c", "#9c3402", "#f57231", "#fc9662"],
+    behavior: behaviors.POWDER,
+    category: "powders",
+    state:  "solid",
+    density: 200,
+};
+elements.Adamantium = {
+    color: ["#a30303", "#5e1111", "#400303", "#451a1a", "#805454"],
+    behavior: behaviors.POWDER,
+    category: "powders",
+    state:  "solid",
+    density: 200,
+};
