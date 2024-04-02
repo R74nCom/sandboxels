c84ecf3ba5b042d8845926d3ea50a3182851a3f3
SquareScreamYT <134925668+SquareScreamYT@users.noreply.github.com>
2024-04-02 09:42:41 +0800

    coral gone :sad: :emoji:

--- mods/aChefsDream.js
+++ mods/aChefsDream.js
@@ -393,6 +393,12 @@ Changelog (v1.12)
 
 
 
+Changelog (v1.12.1)
+	- removed coral, can still be found in ocean.js
+
+
+
+
 */
 
 /*
@@ -1766,77 +1772,6 @@ elements.salmon = {
         "crumb": { elem2:null, chance:0.1, func:behaviors.FEEDPIXEL },
     },
     cutInto: "raw_salmon"
-}
-
-elements.coral_stem = {
-    color: "#4a5e49",
-    behavior: [
-        "CR:coral_stem,coral%2|CR:coral,coral_stem,coral,coral%2|CR:coral_stem,coral%2",
-        "XX|XX|XX",
-        "XX|XX|XX",
-    ],
-    tick: function(pixel) {
-        if (!pixel.fColor) {
-            pixel.fColor = "hsl(" + Math.floor(Math.random()*360) + ",100%,50%)";
-        }
-        var coordsToCheck = [
-            [pixel.x-1,pixel.y],
-            [pixel.x+1,pixel.y],
-            [pixel.x,pixel.y-1],
-            [pixel.x,pixel.y+1],
-        ]
-        for (var i = 0; i < coordsToCheck.length; i++) {
-            var coord = coordsToCheck[i];
-            if (isEmpty(coord[0],coord[1])) {
-                createPixel("coral",coord[0],coord[1]);
-                pixelMap[coord[0]][coord[1]].color = pixel.fColor;
-            }
-        }
-        doDefaults(pixel)
-    },
-    tempHigh: 100,
-    stateHigh: "dead_coral",
-    tempLow: -30,
-    stateLow: "dead_coral",
-    category: "life",
-    burn: 40,
-    burnTime: 50,
-    burnInto: [,"ember","charcoal"],
-    hidden: true,
-    state: "solid",
-    density: 1500,
-    hardness: 0.15,
-    hidden: true,
-}
-
-elements.coral = {
-    color: ["#ff0000","#ff8800","#ffff00","#88ff00","#00ff00","#00ff88","#00ffff","#0088ff","#0000ff","#8800ff","#ff00ff"],
-    behavior: [
-        "XX|XX|XX",
-        "XX|XX|XX",
-        "XX|XX|XX",
-    ],
-    reactions: {
-        "vinegar": { elem1:"dead_plant", elem2:null, chance:0.035 },
-        "baking_soda": { elem1:"dead_plant", elem2:null, chance:0.01 },
-        "bleach": { elem1:"dead_plant", elem2:null, chance:0.05 },
-        "alcohol": { elem1:"dead_plant", elem2:null, chance:0.035}
-    },
-    category:"life",
-    tempHigh: 100,
-    stateHigh: "dead_plant",
-    tempLow: -1.66,
-    stateLow: "frozen_plant",
-    burn:65,
-    burnTime:60,
-    burnInto: "dead_plant",
-    breakInto: "dead_plant",
-    state: "solid",
-    density: 1050,
-    hidden: true,
-    properties:{
-        "colored": false
-    }
 }
 
 elements.raw_salmon = {
