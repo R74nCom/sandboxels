7f73a4f80b7fc7224b17edcdfe117693a525c98c
ThatOtherProto <oliver.nathaniel25611@gmail.com>
2024-04-07 11:51:35 +0100

    New mod! (Not done, just so that it will be easier for me to code

---
+++ mods/Science_mod.js
@@ -0,0 +1,8 @@
+// Do not use
+// This is only a test script for sandboxels modding
+// I'm new to JavaScript and Sandboxels modding
+// So I've created this as a test mod for sandboxels
+// It's gonna have all the elements from the actual mod
+// But it's gonna be really buggy and glitchy (I think)
+
+if (!settings.)
