f059a57239c2b596b49712abd94f179d1081d76d
BionicBison <123780881+Ethanmejia@users.noreply.github.com>
2024-04-10 18:59:22 -0400

    Update Atomic_acid.js

--- mods/Atomic_acid.js
+++ mods/Atomic_acid.js
@@ -1,16 +1,16 @@
 elements.Atomic_Acid = {
     color: "#22ff00",
-    ignore: ["Atomic_Acid", "Atomic_acidcatalyst"],
+    ignore: ["Atomic_Acid"],
     behavior: [
 		"CH:Atomic_Acid|DL AND CH:Atomic_Acid|CH:Atomic_Acid",
-		"DL AND M2 AND CH:Atomic_Acid|XX|DL AND M2 AND CH:Atomic_Acid",
+		"DL AND M2 AND CH:Atomic_Acid|DL%5|DL AND M2 AND CH:Atomic_Acid",
 		"M1 AND CH:Atomic_Acid|DL AND M1 AND CH:Atomic_Acid|M1 AND CH:Atomic_Acid",
 	],
     category: "liquids",
     state:  "liquid",
     density: 1,
     stateHigh: "supernova",
-    tempHigh: 10000,
+    tempHigh: 1000,
     stateLow: "plasma",
     tempLow: -273,
 };
