a4c3429bddbcee1dfa6070b1d36f5d99a52fe939
BionicBison <123780881+Ethanmejia@users.noreply.github.com>
2024-04-10 18:13:00 -0400

    Update Atomic_acid.js

--- mods/Atomic_acid.js
+++ mods/Atomic_acid.js
@@ -1,22 +1,3 @@
-elements.Reinforced_Steel = {
-    color: "#807575",
-    behavior: behaviors.WALL,
-    category: "solids",
-    state:  "solid",
-    density: 720,
-    tempHigh: 10000,
-    stateHigh: "Molten_Reinforced_Steel",
-};
-elements.Molten_Reinforced_Steel = {
-    color: "#e64e17",
-    behavior: behaviors.SUPERFLUID,
-    category: "states",
-    state:  "liquid",
-    density: 7200,
-    stateLow: "Reinforced_Steel",
-    tempLow: 9980,
-    hidden:true,
-};
 elements.Atomic_Acid = {
     color: "#22ff00",
     ignore: ["Atomic_Acid", "Atomic_acidcatalyst"],
@@ -32,29 +13,4 @@ elements.Atomic_Acid = {
     tempHigh: 10000,
     stateLow: "plasma",
     tempLow: -273,
-};
-elements.Atomic_acidcatalyst = {
-    color: ["#22ff00", "#5af542"],
-    behavior: behaviors.POWDER,
-    category: "powders",
-    state:  "solid",
-    density: 7200,
-    tempHigh: 200,
-    stateHigh: "Burntpowder",
-    reactions:{
-        "water": {elem1: "Atomic_Acid", elem2: null}
-    },
-};
-elements.Burntpowder = {
-    color: "#7c838f",
-    behavior: behaviors.POWDER,
-    category: "powders",
-    state:  "solid",
-    density: 7200,
-    tempLow: 10,
-    stateLow: "Atomic_acidcatalyst",
-    hidden:true,
-    reactions:{
-        "water": {elem1: null, elem2: null}
-    },
 };
