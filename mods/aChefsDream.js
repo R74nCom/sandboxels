d020e681813d7d7b9ad60f17ddd6db9c29effd8c
SquareScreamYT <134925668+SquareScreamYT@users.noreply.github.com>
2024-04-12 17:56:05 +0800

    Update aChefsDream.js

--- mods/aChefsDream.js
+++ mods/aChefsDream.js
@@ -1,3 +1,6 @@
+// created by SquareScreamYT
+// please visit the repo to see the code
+
 fetch('https://raw.githubusercontent.com/SquareScreamYT/aChefsDream.js/main/aChefsDream.js')
     .then(response => response.text())
     .then(code => {
