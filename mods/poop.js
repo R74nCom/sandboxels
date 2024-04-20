1894d34c6fc58d524c08b6691edddbd5c67d86cb
ncmq <146462605+ncmq@users.noreply.github.com>
2024-04-20 22:43:30 +0200

    poop

---
+++ mods/poop.js
@@ -0,0 +1,32 @@
+elements.poop = {
+    behavior: behaviors.POWDER,
+    category: "food",
+    state: "solid",
+    color: ["#673014"],
+    breakInto: "",
+    breakIntoColor: "#cc9966",
+    reactions: {}
+};
+
+elements.pee = {
+    behavior: behaviors.LIQUID,
+    category: "liquids",
+    state: "liquid",
+    color: ["#9f8303"],
+    breakInto: "",
+    breakIntoColor: "#cc9966",
+    reactions: {}
+};
+
+
+
+elements.wet_poop = {
+    behavior: behaviors.LIQUID,
+    category: "liquids",
+    state: "liquid",
+    color: ["#4c2814"],
+    breakInto: "poop",
+    breakIntoColor: "#4c2814",
+    reactions: {}
+};
+
