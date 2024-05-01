2beb4b3ecbb9e025ca98066893f0705f608224a4
jakob3797 <167775393+jakob3797@users.noreply.github.com>
2024-05-01 14:49:37 -0500

    Create Quicksand_two.js
    Tested and working

---
+++ Quicksand_two.js
@@ -0,0 +1,272 @@
+elements.dense_quicksand = {
+    color: "#DAA520",
+    behavior: behaviors.LIQUID,
+    viscosity: 1000,
+    tempHigh: 65,
+    stateHigh: "liquid",
+    tempLow: 0,
+    category: "liquids",
+    state: "liquid",
+    density: 1900,
+    stain: 0.2
+};
+
+elements.swamp_mud = {
+    color: "#8B4513",
+    behavior: behaviors.LIQUID,
+    viscosity: 1000,
+    tempHigh: 60,
+    stateHigh: "liquid",
+    tempLow: 0,
+    category: "liquids",
+    state: "liquid",
+    density: 1700,
+    stain: 0.25
+};
+
+elements.wet_mud = {
+    color: "#8B4513",
+    behavior: behaviors.LIQUID,
+    viscosity: 1200,
+    tempHigh: 55,
+    stateHigh: "liquid",
+    tempLow: 0,
+    category: "liquids",
+    state: "liquid",
+    density: 1600,
+    stain: 0.3
+};
+
+elements.wet_quicksand = {
+    color: "#DAA520",
+    behavior: behaviors.LIQUID,
+    viscosity: 700,
+    tempHigh: 70,
+    stateHigh: "liquid",
+    tempLow: 0,
+    category: "liquids",
+    state: "liquid",
+    density: 1800,
+    stain: 0.25
+};
+
+elements.slimy_mud = {
+    color: "#008000",
+    behavior: behaviors.LIQUID,
+    viscosity: 900,
+    tempHigh: 55,
+    stateHigh: "liquid",
+    tempLow: 0,
+    category: "liquids",
+    state: "liquid",
+    density: 1550,
+    stain: 0.3
+};
+
+elements.sticky_mud = {
+    color: "#8B4513",
+    behavior: behaviors.LIQUID,
+    viscosity: 1100,
+    tempHigh: 58,
+    stateHigh: "liquid",
+    tempLow: 0,
+    category: "liquids",
+    state: "liquid",
+    density: 1650,
+    stain: 0.25
+};
+
+elements.sloppy_mud = {
+    color: "#8B4513",
+    behavior: behaviors.LIQUID,
+    viscosity: 1000,
+    tempHigh: 60,
+    stateHigh: "liquid",
+    tempLow: 0,
+    category: "liquids",
+    state: "liquid",
+    density: 1700,
+    stain: 0.3
+};
+
+elements.thick_mud = {
+    color: "#8B4513",
+    behavior: behaviors.LIQUID,
+    viscosity: 1300,
+    tempHigh: 50,
+    stateHigh: "liquid",
+    tempLow: 0,
+    category: "liquids",
+    state: "liquid",
+    density: 1800,
+    stain: 0.35
+};
+
+elements.gloopy_mud = {
+    color: "#8B4513",
+    behavior: behaviors.LIQUID,
+    viscosit: 1950,
+    tempHigh: 56,
+    stateHigh: "liquid",
+    tempLow: 0,
+    category: "liquids",
+    state: "liquid",
+    density: 1600,
+    stain: 0.28
+};
+
+elements.squelchy_mud = {
+    color: "#8B4513",
+    behavior: behaviors.LIQUID,
+    viscosity: 1050,
+    tempHigh: 58,
+    stateHigh: "liquid",
+    tempLow: 0,
+    category: "liquids",
+    state: "liquid",
+    density: 1650,
+    stain: 0.3
+};
+
+elements.mucky_mud = {
+    color: "#8B4513",
+    behavior: behaviors.LIQUID,
+    viscosity: 950,
+    tempHigh: 57,
+    stateHigh: "liquid",
+    tempLow: 0,
+    category: "liquids",
+    state: "liquid",
+    density: 1600,
+    stain: 0.28
+};
+
+elements.soggy_mud = {
+    color: "#8B4513",
+    behavior: behaviors.LIQUID,
+    viscosity: 1050,
+    tempHigh: 58,
+    stateHigh: "liquid",
+    tempLow: 0,
+    category: "liquids",
+    state: "liquid",
+    density: 1650,
+    stain: 0.3
+};
+
+elements.gunky_mud = {
+    color: "#8B4513",
+    behavior: behaviors.LIQUID,
+    viscosity: 900,
+    tempHigh: 55,
+    stateHigh: "liquid",
+    tempLow: 0,
+    category: "liquids",
+    state: "liquid",
+    density: 1550,
+    stain: 0.28
+};
+
+elements.oozy_mud = {
+    color: "#8B4513",
+    behavior: behaviors.LIQUID,
+    viscosity: 950,
+    tempHigh: 56,
+    stateHigh: "liquid",
+    tempLow: 0,
+    category: "liquids",
+    state: "liquid",
+    density: 1600,
+    stain: 0.3
+};
+
+elements.gooey_mud = {
+    color: "#8B4513",
+    behavior: behaviors.LIQUID,
+    viscosity: 1000,
+    tempHigh: 60,
+    stateHigh: "liquid",
+    tempLow: 0,
+    category: "liquids",
+    state: "liquid",
+    density: 1700,
+    stain: 0.3
+};
+
+elements.goopy_mud = {
+    color: "#8B4513",
+    behavior: behaviors.LIQUID,
+    viscosity: 1100,
+    tempHigh: 58,
+    stateHigh: "liquid",
+    tempLow: 0,
+    category: "liquids",
+    state: "liquid",
+    density: 1750,
+    stain: 0.3
+};
+
+elements.mushy_mud = {
+    color: "#8B4513",
+    behavior: behaviors.LIQUID,
+    viscosity: 1000,
+    tempHigh: 60,
+    stateHigh: "liquid",
+    tempLow: 0,
+    category: "liquids",
+    state: "liquid",
+    density: 1700,
+    stain: 0.3
+};
+
+elements.silty_mud = {
+    color: "#8B4513",
+    behavior: behaviors.LIQUID,
+    viscosity: 1050,
+    tempHigh: 58,
+    stateHigh: "liquid",
+    tempLow: 0,
+    category: "liquids",
+    state: "liquid",
+    density: 1650,
+    stain: 0.3
+};
+
+elements.gooey_quicksand = {
+    color: "#DAA520",
+    behavior: behaviors.LIQUID,
+    viscosity: 1750,
+    tempHigh: 70,
+    stateHigh: "liquid",
+    tempLow: 0,
+    category: "liquids",
+    state: "liquid",
+    density: 1800,
+    stain: 0.25
+};
+
+elements.sandy_mud = {
+    color: "#8B4513",
+    behavior: behaviors.LIQUID,
+    viscosity: 1600,
+    tempHigh: 60,
+    stateHigh: "liquid",
+    tempLow: 0,
+    category: "liquids",
+    state: "liquid",
+    density: 170,
+    stain: 0.3
+};
+
+elements.sticky_quicksand = {
+    color: "#DAA520",
+    behavior: behaviors.LIQUID,
+    viscosity: 1700,
+    tempHigh: 65,
+    stateHigh: "liquid",
+    tempLow: 0,
+    category: "liquids",
+    state: "liquid",
+    density: 1800,
+    stain: 0.25
+};
