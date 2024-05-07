8fccf3ea203008649e98ad32a9953c1aebb761a4
jakob3797 <167775393+jakob3797@users.noreply.github.com>
2024-05-07 12:25:26 -0500

    Create E-unstainer.js
    I figured it out and it worked and it is tested updated in a little bit

---
+++ mods/E-unstainer.js
@@ -0,0 +1,28 @@
+elements.eunstainer_2 = {
+    behavior: behaviors.WALL,
+    behaviorOn: [
+        "XX|ST:-1|XX",
+        "XX|ST:-1|XX",
+        "XX|XX|XX",
+        "XX|XX|XX",
+        "XX|XX|XX",
+    ],
+    color: "#4103fc", // Choose your desired color
+    category: "machines",
+    conduct: 1, // This defines how the element conducts electricity or energy, set as needed
+    hardness: 1,
+    state: "solid",
+};
+elements.eunstainer_1 = {
+    behavior: behaviors.WALL,
+    behaviorOn: [ 
+        "XX|ST:-1|XX",
+        "XX|XX|XX",
+        "XX|XX|XX"
+    ],
+    color: "#4103fc", // Choose your desired color
+    category: "machines",
+    conduct: 1, // This defines how the element conducts electricity or energy, set as needed
+    hardness: 1,
+    state: "solid",
+};
