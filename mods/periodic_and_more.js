09d45a1b55f159a90c843517cd585dd3e285de4b
Boxeled <bacquegraysonjames@gmail.com>
2024-07-29 17:29:16 -0500

    Update periodic_and_more.js

--- mods/periodic_and_more.js
+++ mods/periodic_and_more.js
@@ -1,20 +1,10 @@
-// This is how to add a new mod to the game.
 
-// Create a new Javascript file like this one.
-// Add the file to the mods folder on GitHub, or host it somewhere else.
-// https://github.com/R74nCom/sandboxels/tree/main/mods
-
-// To add it in the Mod Loader:
-// If it is in the mods folder, you can just use the name of the file.
-// If it is hosted somewhere else, you can use the full URL, including the HTTPS://.
-
-// Adding elements:
-elements.mayo = {
-    color: "#ffffff",
-    behavior: behaviors.LIQUID,
-    category: "liquids",
+elements.beryllium = {
+    color: "#b3b3b3",
+    behavior: behaviors.WALL,
+    category: "periodic",
     viscosity: 100000,
-    state: "liquid",
+    state: "solid",
     density: 720,
 };
 
