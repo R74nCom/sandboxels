cac0e974d1a93bc873f2444369b6c47513eb7222
Nekonico <163950752+DBNekonico@users.noreply.github.com>
2024-10-03 11:53:01 -0700

    eating

--- mods/genetics.js
+++ mods/genetics.js
@@ -1,25 +1,41 @@
-elements.organism = {}
+elements.organism = {
   color: ["#a19348","#b5a85e"],
   properties: {
-    geneticCode: "00000",
+    geneticCode: 00000,
     food: 0
   },
   tick: function(pixel) {
     if ((pixel.geneticCode & 0b101) = 0) {
       if (tryMove(pixel, pixel.x, pixel.y+1)) {
-        if ((pixel.geneticCode & 0b10) = 1 && Math.random() < 0.1) {
+        if ((pixel.geneticCode & 0b10) > 0 && Math.random() < 0.1) {
           if (!isEmpty(pixel.x,pixel.y+1),true && pixelMap[pixel.x][pixel.y+1].isFood) {
             deletePixel(pixel.x, pixel.y+1);
             pixel.food += 1
           }
         }
+        else if ((pixel.geneticCode & 0b10) = 0 && Math.random() < 0.1) {
+          if (!isEmpty(pixel.x,pixel.y+1),true && pixelMap[pixel.x][pixel.y+1].element === "sugar") {
+            deletePixel(pixel.x, pixel.y+1);
+            pixel.food += 1
+          }
+        }
       }
-      if ((pixel.geneticCode & 0b10) = 1 && Math.random() < 0.1) {
+      if ((pixel.geneticCode & 0b10) > 0 && Math.random() < 0.1) {
         if (!isEmpty(pixel.x+1,pixel.y),true && pixelMap[pixel.x+1][pixel.y].isFood) {
           deletePixel(pixel.x+1, pixel.y);
           pixel.food += 1
         }
         else if (!isEmpty(pixel.x-1,pixel.y),true && pixelMap[pixel.x-1][pixel.y].isFood) {
+          deletePixel(pixel.x-1, pixel.y);
+          pixel.food += 1
+        }
+      }
+      else if ((pixel.geneticCode & 0b10) = 0 && Math.random() < 0.1) {
+        if (!isEmpty(pixel.x+1,pixel.y),true && pixelMap[pixel.x+1][pixel.y].element === "sugar") {
+          deletePixel(pixel.x+1, pixel.y);
+          pixel.food += 1
+        }
+        else if (!isEmpty(pixel.x-1,pixel.y),true && pixelMap[pixel.x-1][pixel.y].element === "sugar") {
           deletePixel(pixel.x-1, pixel.y);
           pixel.food += 1
         }
