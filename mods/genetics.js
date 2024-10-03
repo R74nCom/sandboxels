101c2d29396887a5b58c96ef9c8d63f5ff7cff16
Nekonico <163950752+DBNekonico@users.noreply.github.com>
2024-10-03 09:24:41 -0700

    genetics mod template

---
+++ mods/genetics.js
@@ -0,0 +1,21 @@
+elements.organism = {}
+  color: ["#a19348","#b5a85e"],
+  properties: {
+    "geneticCode": "00000"
+  },
+  tick: function(pixel) {
+    if ((pixel.geneticCode & 0b101) = 0) {
+      
+    }
+    else if ((pixel.geneticCode & 0b101) = 1) {
+      
+    }
+  },
+  tempHigh: 150,
+  stateHigh: "meat",
+  tempLow: -50,
+  stateLow: "frozen_meat",
+  category: "life",
+  state: "solid",
+  density: 1050
+},
