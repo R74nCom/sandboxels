b6b349b699c1eaef44ba808b8b903950bbc1eae1
Fivey1777 <oliver.ng777@gmail.com>
2025-01-19 19:43:29 +0800

    Update fun.js

--- mods/fun.js
+++ mods/fun.js
@@ -1,25 +1,25 @@
 //Help me find bugs in my code, ok? I am just doing another test
-elements.sand.name = "EEEEEEEEEEE_pick up my ass c0ck loldoewockecooffdsvifsjvjfso",
+elements.sand.name = "EEEEEEEEEEE_pick up my ass c0ck loldoewockecooffdsvifsjvjfso";
 elements.sand.behavior = behaviors.WALL;
 
-elements.water.name = "NEWater",
+elements.water.name = "NEWater";
 elements.water.behavior = behaviors.POWDER;
 
-elements.unknown.name = "Player 456",
+elements.unknown.name = "Player 456";
 elements.unknown.behavior = behaviors.POWDER;
 
-elements.dirty_water.name = "Water",
+elements.dirty_water.name = "Water";
 elements.dirty_water.behavior = behaviors.WALL;
 
-elements.paint.name = "Gibberish Typer",
+elements.paint.name = "Gibberish Typer";
 elements.paint.behavior = behaviors.LIQUID;
 
-elements.primordial_soup.name = "annoying_space",
+elements.primordial_soup.name = "annoying_space"; 
 elements.primordial_soup.behavior = behaviors.WALL;
 
 //I want to make this a real element soon
-elements.diamond.name = "Gravitite",
-elements.diamond.behavior = behaviors.AGPOWDER,
+elements.diamond.name = "Gravitite";
+elements.diamond.behavior = behaviors.AGPOWDER;
 elements.diamond.color = ["#f7adda","#ff80f2","#c421a3","#981a92","#ec32e3","#f062f0"];
 
 elements.permafrost.name = "Frozen_testicle";
