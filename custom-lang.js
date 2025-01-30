dec131e49d2cb2335916896288bc31ffeb27e9ce
wowozui <159249781+wowozui@users.noreply.github.com>
2025-01-30 13:28:33 +0100

    Create custom-lang.js

---
+++ custom-lang.js
@@ -0,0 +1,30 @@
+// Define the language code and name
+languageList.push("custom");
+languageNames.custom = "Custom Language";
+
+// Create translations object
+elements.names.custom = {
+    "water": "Water",
+    "fire": "Fire",
+    "earth": "Earth",
+    "air": "Air",
+    "sand": "Sand",
+    "stone": "Stone",
+    "lava": "Lava",
+    "steam": "Steam"
+};
+
+// Create category translations
+categories.names.custom = {
+    "powders": "Powders",
+    "liquids": "Liquids",
+    "gases": "Gases",
+    "solids": "Solids"
+};
+
+// Create interface translations
+strings.custom = {
+    "loading": "Loading...",
+    "pauseMessage": "Game Paused",
+    "menuMessage": "Main Menu"
+};
