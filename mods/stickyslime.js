6351e82d5b0ed3da2976f56d48a87b8749f832f7
Suss <167750109+therealsuss@users.noreply.github.com>
2025-02-24 17:04:37 +1100

    stickyslime is gone, say hello to stickystuff
    yeah, its not just slime that is sticky anymore so im forced to rename it

--- mods/stickyslime.js
+++
@@ -1,5 +0,0 @@
-elements.slime.behavior = [
-    "XX|ST|XX",
-    "ST AND M2|XX|ST AND M2",
-    "XX|ST AND M1|XX",
-];
---
+++ mods/stickystuff.js
@@ -0,0 +1,59 @@
+elements.slime.behavior = [
+    "XX|ST|XX",
+    "ST AND M2|XX|ST AND M2",
+    "XX|ST AND M1|XX",
+];
+
+elements.honey.behavior = [
+    "XX|ST|XX",
+    "ST AND M2|XX|ST AND M2",
+    "XX|ST AND M1|XX",
+];
+
+elements.jelly.behavior = [
+    "XX|ST|XX",
+    "ST AND M2|XX|ST AND M2",
+    "XX|ST AND M1|XX",
+];
+
+elements.sap.behavior = [
+    "XX|ST|XX",
+    "ST AND M2|XX|ST AND M2",
+    "XX|ST AND M1|XX",
+];
+
+elements.caramel.behavior = [
+    "XX|ST|XX",
+    "ST AND M2|XX|ST AND M2",
+    "XX|ST AND M1|XX",
+];
+
+elements.melted_chocolate.behavior = [
+    "XX|ST|XX",
+    "ST AND M2|XX|ST AND M2",
+    "XX|ST AND M1|XX",
+];
+
+elements.melted_cheese.behavior = [
+    "XX|ST|XX",
+    "ST AND M2|XX|ST AND M2",
+    "XX|ST AND M1|XX",
+];
+
+elements.melted_wax.behavior = [
+    "XX|ST|XX",
+    "ST AND M2|XX|ST AND M2",
+    "XX|ST AND M1|XX",
+];
+
+elements.batter.behavior = [
+    "XX|ST|XX",
+    "ST AND M2|XX|ST AND M2",
+    "XX|ST AND M1|XX",
+];
+
+elements.nut_butter.behavior = [
+    "XX|ST|XX",
+    "ST AND M2|XX|ST AND M2",
+    "XX|ST AND M1|XX",
+];
