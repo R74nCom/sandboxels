a1c771158cfa662ec75a92517ad47a9b9181c06a
bradley-dev777 <hu.yu@wcs-g.com>
2025-03-17 17:31:27 -0600

    Update deleters.js

--- mods/deleters.js
+++ mods/deleters.js
@@ -5,6 +5,16 @@ elements.voidbomb = {
     "XX|XX|XX",
     "XX|EX:5>void|XX"
   ]
-  category = "weapons"
+  category = "voids"
   state = "powder"
 }
+elements.powdervoid = elements.void
+elements.powdervoid.state = "powder"
+elements.powdervoid.category = "voids"
+elements.void.category = "voids"
+elements.liquidvoid = elements.void
+elements.liquidvoid.state = "liquid"
+elements.liquidvoid.category = "voids"
+elements.voidgas = elements.void
+elements.voidgas.state = "gas"
+elements.voidgas.category = "voids"
