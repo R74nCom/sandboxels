bafdda7e4c6c42ee13340b64a3267f3d0e8cc16d
bradley-dev777 <hu.yu@wcs-g.com>
2025-03-17 18:07:18 -0600

    my first mod yay

--- mods/deleters.js
+++ mods/deleters.js
@@ -1,20 +1,24 @@
 elements.voidbomb = {
-  color = "#101010"
-  behavior = [
+  color: "#FFFFFF",
+  behavior: [
     "XX|XX|XX",
     "XX|XX|XX",
-    "XX|EX:5>void|XX"
-  ]
-  category = "voids"
-  state = "powder"
-}
-elements.powdervoid = elements.void
-elements.powdervoid.state = "powder"
-elements.powdervoid.category = "voids"
-elements.void.category = "voids"
-elements.liquidvoid = elements.void
-elements.liquidvoid.state = "liquid"
-elements.liquidvoid.category = "voids"
-elements.voidgas = elements.void
-elements.voidgas.state = "gas"
-elements.voidgas.category = "voids"
+    "XX|EX:5>void AND M1|XX"
+  ],
+  category: "voids",
+  state: "powder"
+};
+elements.powdervoid = elements.void;
+elements.powdervoid.state = "powder";
+elements.powdervoid.category = "voids";
+elements.powdervoid.behavior = behaviors.POWDER;
+elements.void.category = "voids";
+elements.void.behavior = behaviors.WALL;
+elements.liquidvoid = elements.void;
+elements.liquidvoid.state = "liquid";
+elements.liquidvoid.category = "voids";
+elements.liquidvoid.behavior = behaviors.LIQUID;
+elements.voidgas = elements.void;
+elements.voidgas.state = "gas";
+elements.voidgas.category = "voids";
+elements.voidgas.behavior = behaviors.GAS;
