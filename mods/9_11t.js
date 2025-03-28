57752346aa03f604a4b43993b205b6ca32b6ab9c
jakob3797 <167775393+jakob3797@users.noreply.github.com>
2025-03-28 10:57:53 -0500

    Create 9_11t.js
    tested and worked

---
+++ mods/9_11t.js
@@ -0,0 +1,43 @@
+elements.twin_towers = {
+  color: "#8A4D24",
+  behavior: [
+    "XX|XX|CR:wal|XX|XX|XX|XX|XX|XX|CR:wal|XX|XX",
+    "XX|DL|CR:wal|XX|XX|XX|XX|XX|XX|CR:wal|XX|XX",
+    "XX|CR:wal|CR:wal|CR:wal|XX|XX|XX|XX|CR:wal|CR:wal|CR:wal|XX",
+    "XX|CR:wal|CR:wal|CR:wal|XX|XX|XX|XX|CR:wal|CR:wal|CR:wal|XX",
+    "XX|CR:wal|CR:wal|CR:wal|XX|XX|XX|XX|CR:wal|CR:wal|CR:wal|XX",
+    "CR:wal|CR:wal|CR:wal|CR:wal|CR:wal|XX|XX|CR:wal|CR:wal|CR:wal|CR:wal|CR:wal",
+    "CR:wal|CR:wal|CR:wal|CR:wal|CR:wal|XX|XX|CR:wal|CR:wal|CR:wal|CR:wal|CR:wal",
+    "CR:wal|CR:wal|CR:wal|CR:wal|CR:wal|XX|XX|CR:wal|CR:wal|CR:wal|CR:wal|CR:wal",
+    "CR:wal|CR:wal|CR:wal|CR:wal|CR:wal|XX|XX|CR:wal|CR:wal|CR:wal|CR:wal|CR:wal",
+    "CR:wal|CR:wal|CR:wal|CR:wal|CR:wal|XX|XX|CR:wal|CR:wal|CR:wal|CR:wal|CR:wal",
+    "CR:wal|CR:wal|CR:wal|CR:wal|CR:wal|XX|XX|CR:wal|CR:wal|CR:wal|CR:wal|CR:wal",
+    "CR:wal|CR:wal|CR:wal|CR:wal|CR:wal|XX|XX|CR:wal|CR:wal|CR:wal|CR:wal|CR:wal",
+    "CR:wal|CR:wal|CR:wal|CR:wal|CR:wal|XX|XX|CR:wal|CR:wal|CR:wal|CR:wal|CR:wal",
+    "CR:wal|CR:wal|CR:wal|CR:wal|CR:wal|XX|XX|CR:wal|CR:wal|CR:wal|CR:wal|CR:wal",
+    "CR:wal|CR:wal|CR:wal|CR:wal|CR:wal|XX|XX|CR:wal|CR:wal|CR:wal|CR:wal|CR:wal",
+    "CR:wal|CR:wal|CR:wal|CR:wal|CR:wal|XX|XX|CR:wal|CR:wal|CR:wal|CR:wal|CR:wal",
+    "CR:wal|CR:wal|CR:wal|CR:wal|CR:wal|XX|XX|CR:wal|CR:wal|CR:wal|CR:wal|CR:wal",
+    "CR:wal|CR:wal|CR:wal|CR:wal|CR:wal|XX|XX|CR:wal|CR:wal|CR:wal|CR:wal|CR:wal",
+    "CR:wal|CR:wal|CR:wal|CR:wal|CR:wal|XX|XX|CR:wal|CR:wal|CR:wal|CR:wal|CR:wal",
+    "CR:wal|CR:wal|CR:wal|CR:wal|CR:wal|XX|XX|CR:wal|CR:wal|CR:wal|CR:wal|CR:wal",
+    "CR:wal|CR:wal|CR:wal|CR:wal|CR:wal|XX|XX|CR:wal|CR:wal|CR:wal|CR:wal|CR:wal",
+    "CR:wal|CR:wal|CR:wal|CR:wal|CR:wal|XX|XX|CR:wal|CR:wal|CR:wal|CR:wal|CR:wal",
+    "CR:wal|CR:wal|CR:wal|CR:wal|CR:wal|XX|XX|CR:wal|CR:wal|CR:wal|CR:wal|CR:wal",
+    "CR:wal|CR:wal|CR:wal|CR:wal|CR:wal|XX|XX|CR:wal|CR:wal|CR:wal|CR:wal|CR:wal",
+    "CR:wal|CR:wal|CR:wal|CR:wal|CR:wal|XX|XX|CR:wal|CR:wal|CR:wal|CR:wal|CR:wal",
+    "CR:wal|CR:wal|CR:wal|CR:wal|CR:wal|XX|XX|CR:wal|CR:wal|CR:wal|CR:wal|CR:wal",
+  ],
+  category: "tower",
+  viscosity: 1,
+  state: "solid",
+  density: 43
+};
+
+elements.wal = {
+  behavior: behaviors.SOLID,
+  color: "#6d6d6d",
+  category: "tower",
+  state: "solid",
+  hidden: false
+};
