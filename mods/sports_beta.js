fd56ba45ca5ab77bb17aa5ff7be0fba2c0c0c51c
Nicholas <mrneko946@gmail.com>
2025-03-31 12:58:35 -0400

    Update sports_beta.js

--- mods/sports_beta.js
+++ mods/sports_beta.js
@@ -2,61 +2,68 @@ elements.softball = {
 	color: "#ffe300",
 	behavior: behaviors.BOUNCY,
 	category: "softball",
-	state: "solid",
+	state: "solid"
 };
 
 elements.baseball = {
 	color: "#eae7dc",
 	behavior: behaviors.BOUNCY,
 	category: "baseball",
-	state: "solid",
+	state: "solid"
 };
 
 elements.basketball = {
 	color: "#e77f1c",
 	behavior: behaviors.BOUNCY,
 	category: "basketball",
-	state: "solid",
+	state: "solid"
 };
 
 elements.football = {
 	color: "#ac5541",
 	behavior: behaviors.BOUNCY,
 	category: "football",
-	state: "solid",
+	state: "solid"
 };
 
 elements.soccer_ball = {
 	color: "#f9fbfa",
 	behavior: behaviors.BOUNCY,
 	category: "soccer",
-	state: "solid",
+	state: "solid"
 };
 
 elements.tennis_ball = {
-    color: "#9ab973",
-    behavior: behaviors.BOUNCY,
-    category: "tennis",
-    state: "solid",
+	color: "#9ab973",
+	behavior: behaviors.BOUNCY,
+	category: "tennis",
+	state: "solid"
 };
 
 elements.puck = {
-    color: "#180902",
-    behavior: behaviors.BOUNCY,
-    category: "hockey",
-    state: "solid",
+	color: "#180902",
+	behavior: behaviors.BOUNCY,
+	category: "hockey",
+	state: "solid"
 };
 
 elements.dodge_ball = {
-    color: "#533d61",
-    behavior: behaviors.BOUNCY,
-    category: "gaga_ball",
-    state: "solid",
+	color: "#533d61",
+	behavior: behaviors.BOUNCY,
+	category: "gaga ball",
+	state: "solid"
+};
+
+elements.volleyball = {
+	color: "#e2dcf2",
+	behavior: behaviors.BOUNCY,
+	category: "volleyball",
+	state: "solid"
 };
 
 elements.deodorant = {
-   color: "#def2fc",
-   behavior: behaviors.DGAS
-   category: "gases",
-   state: "gas",
+	color: "#def2fc",
+	behavior: behaviors.DGAS,
+	category: "gases",
+	state: "gas"
 };
