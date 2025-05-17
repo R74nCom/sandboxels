bd92258955f9d191040c1f2e30395e4202dfc0b5
Suss <167750109+therealsuss@users.noreply.github.com>
2025-05-17 17:04:45 +1000

    those pixel variants that are for a limited time but they're mods i gues
    yeah

---
+++ mods/CandyCorn.js
@@ -0,0 +1,8 @@
+elements.candy.colorPattern = [
+		"W","W","W","W","W",
+		"O","O","O","O","O",
+		"Y","Y","Y","Y","Y",
+	]
+	elements.candy.colorKey = {"W":"#ffffff", "Y":"#ffcd55", "O":"#ff9933"}
+	elements.candy.color = ["#ffffff","#ffffff","#ff9933","#ff9933","#ffcd55","#ffcd55"]
+	elements.candy.breakIntoColor = ["#ffffff","#ffcd55","#ff9933"]
