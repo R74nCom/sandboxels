f24becdd292699f991d39994200e654ffaf53136
tobycoding-html <tobythedev1229@gmail.com>
2025-06-08 14:54:27 -0500

    Reactor_Supplies.js
    This is a addon to make reactors.

--- mods/reactor_supplies_and_others.js
+++ mods/reactor_supplies_and_others.js
@@ -1,4 +1,4 @@
-elements.reactorwater = {
+elements.reactor_water = {
 	color: "#00FFCB",
 	behavior: behaviors.LIQUID,
 	category: "reactor_supplies",
@@ -6,11 +6,11 @@ elements.reactorwater = {
 	density: 1050,
 	tempHigh:200,
 	tempLow:-40,
-    stateHigh: "Reactor_Water_Steam",
-	stateLow: "Reactor_Water_Ice",
+    stateHigh: "reactor_water_steam",
+	stateLow: "reactor_water_ice",
 };
 
-elements.reactorcontainerbodyuser = {
+elements.reactor_container_body_user = {
 	color: "#676767",
 	behavior: behaviors.SOLID,
 	category: "reactor_supplies",
@@ -22,27 +22,27 @@ elements.reactorcontainerbodyuser = {
 	stateLow: "Frozen_Reactor_Container",
 };
 
-elements.moltenreactorcontainer = {
+elements.molten_reactor_container = {
 	color: "#FF5000",
 	behavior: behaviors.LIQUID,
 	category: "reactor_supplies",
 	state: "liquid",
 	tempLow: 50,
 	temp: 700,
-	stateLow: "Reactor_Container_Body_User",
+	stateLow: "reactor_container_body_user",
 };
 
-elements.frozenreactorcontainer = {
+elements.frozen_reactor_container = {
 	color: "#7FA0A8",
 	behavior: behaviors.SOLID,
 	category: "reactor_supplies",
 	state: "solid",
 	tempHigh: 50,
 	temp: -200,
-	stateHigh: "Reactor_Container_Body_User",
+	stateHigh: "reactor_container_body_user",
 };
 
-elements.reactorwatersteam = {
+elements.reactor_water_steam = {
 	color: "#8CF193",
 	behavior: behaviors.GAS,
 	category: "reactor_supplies",
@@ -52,36 +52,36 @@ elements.reactorwatersteam = {
 	stateLow: "Reactor_Water",
 };
 
-elements.reactorwaterice = {
+elements.reactor_water_ice = {
 	color: "#BBF5E6",
 	behavior: behaviors.SOLID,
 	category: "reactor_supplies",
 	state: "solid",
 	tempHigh: 20,
-	stateHigh: "Reactor_Water",
+	stateHigh: "reactor_water",
 	temp: -40,
 };
 
-elements.thefuntool = {
+elements.the_fun_tool = {
 	color: "#FF0000",
 	category: "reactor_supplies",
 	tool: function(pixel) {
-		if (pixel.elemet == "reactorwater") {
+		if (pixel.elemet == "reactor_water") {
 			pixel.element = "explosion";
 		};
-		if (pixel.element == "reactorwaterice") {
+		if (pixel.element == "reactor_water_ice") {
 			pixel.element = "explosion";
 		}; 
-		if (pixel.element == "reactorwatersteam") {
+		if (pixel.element == "reactor_water_steam") {
 			pixel.element = "explosion";
 		};
-		if (pixel.element == "moltenreactorcontainer") {
+		if (pixel.element == "molten_reactor_container") {
 			pixel.element = "explosion";
 		};
-		if (pixel.element == "frozenreactorcontainer") {
+		if (pixel.element == "frozen_reactor_container") {
 			pixel.element = "explosion";
 		};
-		if (pixel.element == "reactorcontainerbodyuser") {
+		if (pixel.element == "reactor_container_body_user") {
 			pixel.element = "explosion";
 		};
 	}
