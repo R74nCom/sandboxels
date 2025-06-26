51f2345f50418f4c801c19007fa6028a4e436545
Blix4er <Elijahrobertwade@gmail.com>
2025-06-25 21:30:10 -0400

    Add files via upload

---
+++ glorgelems.js
@@ -0,0 +1,9 @@
+elements.glorg_bits = {
+	color: "#8fff5c",
+	behavior: behaviors.POWDER,
+	category: "life",
+	state: "solid",
+  	reactions: {
+        	"glorg_bits": { elem1: :"glorg_bits", elem2: "glorg_bits" },
+    },
+}
