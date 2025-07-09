fc63c7ff621435b64e494d2a91b3308aca7320c5
personiguess-a <slitherlightalt@gmail.com>
2025-07-09 13:55:22 -0400

    Update sandboxelse.js

--- mods/sandboxelse.js
+++ mods/sandboxelse.js
@@ -1,15 +1,10 @@
-elements.ghire_normal = {
-	color: "#eb8334",
-	behavior: behaviors.WALL,
-	category: "land",
-	state: "solid",
-};
-// Adding elements:
 elements.ghire = {
-    color: "#eb8334",
-    behavior: behaviors.LIQUID,
-    category: "liquids",
-    viscosity: 100000,
-    state: "liquid",
-    density: 720,
+  color: ["#eb8334","#ffd8c9","#ffeda3","#d98938","#000000"],
+  behavior: behaviors.POWDER,
+  category: "powders",
+  state: "solid",
+  density: 265,
+  burn: 1000,
+  burnTime: 300,
+  burnInto: ["carbon_dioxide"],
 };
