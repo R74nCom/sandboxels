2b14c949883a81a2a989c438bf2a1fa6808a293b
personiguess-a <slitherlightalt@gmail.com>
2025-07-09 12:50:01 -0400

    Update sandboxelse.js

--- mods/sandboxelse.js
+++ mods/sandboxelse.js
@@ -4,3 +4,12 @@ elements.ghire_normal = {
 	category: "land",
 	state: "solid",
 }
+// Adding elements:
+elements.ghire = {
+    color: "#eb8334",
+    behavior: behaviors.LIQUID,
+    category: "liquids",
+    viscosity: 100000,
+    state: "liquid",
+    density: 720,
+};
