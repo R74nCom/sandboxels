fcc5cc35ea4b94bd1a9f4c4cae6f4160c4c99096
Suss <167750109+therealsuss@users.noreply.github.com>
2025-07-23 10:43:16 +1000

    fix

--- mods/petal_dye.js
+++ mods/petal_dye.js
@@ -6,3 +6,35 @@ elements.water.reactions.petal = {
     }
   }
 }
+elements.salt_water.reactions.petal = {
+  func: function(pixel1, pixel2) {
+    if (pixel1.temp > 64) {
+      deletePixel(pixel1.x, pixel1.y);
+      pixel2.element = "dye";
+    }
+  }
+}
+elements.sugar_water.reactions.petal = {
+  func: function(pixel1, pixel2) {
+    if (pixel1.temp > 64) {
+      deletePixel(pixel1.x, pixel1.y);
+      pixel2.element = "dye";
+    }
+  }
+}
+elements.seltzer.reactions.petal = {
+  func: function(pixel1, pixel2) {
+    if (pixel1.temp > 64) {
+      deletePixel(pixel1.x, pixel1.y);
+      pixel2.element = "dye";
+    }
+  }
+}
+elements.pool_water.reactions.petal = {
+  func: function(pixel1, pixel2) {
+    if (pixel1.temp > 64) {
+      deletePixel(pixel1.x, pixel1.y);
+      pixel2.element = "dye";
+    }
+  }
+}
