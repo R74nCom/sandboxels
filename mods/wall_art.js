e048d8ad8156f7667d49d13ba654aaa6aa326c5c
A Crazy Pencil <157081760+pizzapi2012@users.noreply.github.com>
2025-08-16 14:08:13 -0500

    Create wall_art.js

---
+++ mods/wall_art.js
@@ -0,0 +1,13 @@
+/*
+A mod that adds the element "wall_art" (in solids)
+The use is to use it with image as a element.
+(c) ACrazyPencil 2025
+*/
+
+elements.wall_art = {
+    name: "Wall Art",
+    singleColor: true,
+    state: "solid",
+    behavior: behaviors.WALL,
+    category: "solids"
+}
