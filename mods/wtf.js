966fb325178a488076b3516d020c871d3c24a5e5
Jack <jackedmunds2013@gmail.com>
2026-07-26 13:31:31 +0100

    Delete mods/wtf.js

--- mods/wtf.js
+++
@@ -1,3 +0,0 @@
-if (!elements.soapy_water) elements.soapy_water = {};
-if (!elements.soapy_water.reactions) elements.soapy_water.reactions = {};
-elements.soapy_water.reactions.ketchup = { "elem1": "soapy_water", "elem2": "milk" };
