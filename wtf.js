c4c5142c5a6153d99149db02922177de69e8d042
Jack <jackedmunds2013@gmail.com>
2026-07-26 13:19:23 +0100

    Wrap salt_water reactions initialization in runAfterLoad

--- wtf.js
+++ wtf.js
@@ -1,3 +1,12 @@
-if (!elements.salt_water) elements.salt_water = {};
-if (!elements.salt_water.reactions) elements.salt_water.reactions = {};
-elements.salt_water.reactions.ketchup = { "elem1": "salt_water", "elem2": "milk" };
+runAfterLoad(function() {
+
+    if (elements.salt_water) {
+        if (!elements.salt_water.reactions) elements.salt_water.reactions = {};
+        elements.salt_water.reactions.ketchup = { "elem1": "salt_water", "elem2": "milk" };
+    }
+
+    if (elements.ketchup) {
+        if (!elements.ketchup.reactions) elements.ketchup.reactions = {};
+        elements.ketchup.reactions.salt_water = { "elem1": "milk", "elem2": "salt_water" };
+    }
+});
