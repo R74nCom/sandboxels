9f06a4f6f01d63298a6bf31ea818eeff935a8379
Jack <jackedmunds2013@gmail.com>
2026-07-26 13:11:18 +0100

    Replace soapy_water with salt_water in reactions

--- wtf.js
+++ wtf.js
@@ -1,3 +1,3 @@
-if (!elements.soapy_water) elements.soapy_water = {};
-if (!elements.soapy_water.reactions) elements.soapy_water.reactions = {};
-elements.soapy_water.reactions.ketchup = { "elem1": "soapy_water", "elem2": "milk" };
+if (!elements.salt_water) elements.salt_water = {};
+if (!elements.salt_water.reactions) elements.salt_water.reactions = {};
+elements.salt_water.reactions.ketchup = { "elem1": "salt_water", "elem2": "milk" };
