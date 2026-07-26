a93b4ea83bbc73d466db166c992bde6e9a4db293
Jack <jackedmunds2013@gmail.com>
2026-07-26 13:08:48 +0100

    Initialize soapy_water reactions in wtf.js

---
+++ wtf.js
@@ -0,0 +1,3 @@
+if (!elements.soapy_water) elements.soapy_water = {};
+if (!elements.soapy_water.reactions) elements.soapy_water.reactions = {};
+elements.soapy_water.reactions.ketchup = { "elem1": "soapy_water", "elem2": "milk" };
